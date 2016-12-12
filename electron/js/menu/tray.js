49c0c2dff785484a2f4df48a81c9f5eb935b3932
Panayiotis Lipiridis <lipiridis@gmail.com>
2016-12-12 13:15:33 +0100

    Fixed linux test

--- electron/js/menu/tray.js
+++ electron/js/menu/tray.js
@@ -62,13 +62,13 @@ module.exports = {
   updateBadgeIcon: function(win) {
     setTimeout(() => {
       let counter = (/\(([0-9]+)\)/).exec(win.getTitle() || (win.webContents ? win.webContents.getTitle() : ''));
-      let count = parseInt(counter ? counter[1] : '0', 10);
+      let count = parseInt(counter ? counter[1] : 0, 10);
       if (count) {
         this.useBadgeIcon();
       } else {
         this.useDefaultIcon();
       }
-      win.setOverlayIcon(count && process.platform !== 'darwin' ? iconOverlayPath : null, locale.getText('unreadMessages'));
+      win.setOverlayIcon(count && process.platform === 'win32' ? iconOverlayPath : null, locale.getText('unreadMessages'));
       win.flashFrame(!win.isFocused() && count > lastUnreadCount);
       app.setBadgeCount(count);
       lastUnreadCount = count;
