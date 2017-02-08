5bc106ca8157b497313b1480042138c082fa8254
Conor Anderson <conor@conr.ca>
2017-02-08 15:07:04 -0500

    Use `showPrimaryWindow()` in `main.js`

--- electron/main.js
+++ electron/main.js
@@ -35,6 +35,7 @@ const systemMenu = require('./js/menu/system');
 const developerMenu = require('./js/menu/developer');
 const tray = require('./js/menu/tray');
 const util = require('./js/util');
+const windowManager = require('./js/window-manager.js');
 
 const APP_PATH = app.getAppPath();
 const PRELOAD_JS = path.join(APP_PATH, 'js', 'preload.js');
@@ -74,10 +75,7 @@ if (config.DEVELOPMENT) {
 if (process.platform !== 'darwin') {
   shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
     if (main) {
-      if (!main.isVisible()) {
-        main.show();
-      }
-      main.focus();
+      windowManager.showPrimaryWindow();
     }
     return true;
   });
@@ -125,7 +123,7 @@ ipcMain.on('save-picture', function(event, fileName, bytes) {
 });
 
 ipcMain.on('notification-click', function() {
-  main.show();
+  windowManager.showPrimaryWindow();
 });
 
 ipcMain.on('google-auth-request', function(event) {
