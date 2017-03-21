2392e0d736042b3788a328678315d4f316a186c9
Sven Jost <sven@wire.com>
2017-03-21 15:22:51 +0100

    Fix issue with print

--- bin/win-prod-s3.py
+++ bin/win-prod-s3.py
@@ -33,7 +33,7 @@ info_json = os.path.join(bin_root, '..', 'info.json')
 
 def upload_file(source, dest):
   if not os.path.isfile(source):
-    print '%s not found'
+    print '%s not found' % source
     return
 
   print 'Uploading %s to %s' % (os.path.basename(source), dest),
