07ef3929ff013d0594b444d889ff3afd04494123
Florian <florian.keller@wire.com>
2017-04-11 14:28:34 +0200

    Only use gpg 2.0.x + Add better logging

--- bin/repo/linux-prod-repo.sh
+++ bin/repo/linux-prod-repo.sh
@@ -2,38 +2,55 @@
 
 set -eu -o pipefail
 
+SCRIPT_NAME="${0##*/}"
+SCRIPT_DIR="${0%/*}"
+
 BINARY_DIR="./debian/pool/main"
 STABLE_DIR="./debian/dists/stable"
 CACHE_DIR="./cache"
-CONF_DIR="../../bin/repo/conf"
+CONF_DIR="${SCRIPT_DIR}/conf"
 
 APT_CONF_FILE="${CONF_DIR}/apt-ftparchive.conf"
 RELEASE_FILE="${STABLE_DIR}/Release"
 STABLE_CONF_FILE="${CONF_DIR}/stable.conf"
 
+GPG_TEMP_DIR=".gpg-temporary"
 PGP_SIGN_ID="D599C1AA126762B1"
 PGP_KEYFILE="${PGP_PRIVATE_KEY_FILE:-${PGP_SIGN_ID}.asc}"
-GPG_TEMP_DIR=".gpg-temporary"
+PGP_PASSPHRASE="${PGP_PASSPHRASE:-""}"
 
 _command_exist() {
   command -v "${1}" > /dev/null
 }
 
+_log() {
+  echo "${SCRIPT_NAME}: ${1}"
+}
+
+_error() {
+  echo >&2 "${SCRIPT_NAME}: Error: ${1}"
+}
+
 mkdir -p "${BINARY_DIR}" "${STABLE_DIR}/main/binary-"{all,i386,amd64} "${CONF_DIR}" "${CACHE_DIR}"
 
-if ! _command_exist "gpg"; then
-  echo >&2 "Could not find gpg. Please install package 'gnupg2'."
+if _command_exist "gpg2"; then
+  GPG_VERSION="$(gpg2 --version | head -n 1 | grep -oE '2\..*')"
+  if ! grep -qE "2\.0" <<< "${GPG_VERSION}"; then
+    _error "Your gpg2 version is ${GPG_VERSION}. Signing with a predefined passphrase will work only with gpg2 version 2.0.x."
+    exit 1
+  fi
+else
+  _error "Could not find gpg2. Please install package 'gnupg2' version 2.0.x."
   exit 1
 fi
 
 if ! _command_exist "apt-ftparchive"; then
-  echo >&2 "Could not find apt-ftparchive. Please install package 'apt-utils'."
+  _error "Could not find apt-ftparchive. Please install package 'apt-utils'."
   exit 1
 fi
 
-# Check if .deb files exist in this folder
-if ! ls *.deb > /dev/null 2>&1; then
-  echo >&2 "No binaries found. Add some in ${PWD}."
+if ! ls ./*.deb > /dev/null 2>&1; then
+  _error "No binaries found. Add some in ${PWD}."
   exit 1
 fi
 
@@ -42,51 +59,52 @@ for FILE in *.deb; do
 done
 
 if [ ! -r "${APT_CONF_FILE}" ]; then
-  echo >&2 "Could not read ${APT_CONF_FILE}. Create it in ${CONF_DIR}."
+  _error "Could not read ${APT_CONF_FILE}. Create it in ${CONF_DIR}."
   exit 1
 fi
 
 if [ ! -r "${STABLE_CONF_FILE}" ]; then
-  echo >&2 "Could not read ${STABLE_CONF_FILE}. Create it in ${CONF_DIR}."
+  _error "Could not read ${STABLE_CONF_FILE}. Create it in ${CONF_DIR}."
   exit 1
 fi
 
 # Check if private key file exists
 if [ ! -r "${PGP_KEYFILE}" ]; then
-  echo >&2 "PGP Key file '${PGP_KEYFILE}' missing. Please add it to ${PWD}."
+  _error "PGP key file '${PGP_KEYFILE}' missing. Please add it to ${PWD}."
   exit 1
 fi
 
 if [ -z "${PGP_PASSPHRASE}" ]; then
-  echo >&2 "You need to set PGP_PASSPHRASE in order to sign the Release file."
+  _error "You need to set PGP_PASSPHRASE in order to sign the release file."
   exit 1
 fi
 
-echo "Reading config file..."
+_log "Reading config file..."
 apt-ftparchive generate "${APT_CONF_FILE}"
 
-echo "Generating release files..."
+_log "Generating release files..."
 apt-ftparchive -c "${STABLE_CONF_FILE}" release "${STABLE_DIR}" > "${RELEASE_FILE}"
 
-echo "Signing release files..."
+_log "Signing release files..."
 mkdir -p "${GPG_TEMP_DIR}"
 chmod 700 "${GPG_TEMP_DIR}"
 
 gpg2 --batch \
-    --homedir "${GPG_TEMP_DIR}" \
-    --import "${PGP_KEYFILE}"
+     --homedir "${GPG_TEMP_DIR}" \
+     --import "${PGP_KEYFILE}"
 
 echo "${PGP_PASSPHRASE}" | \
-gpg2 --passphrase-fd 0 \
-    --homedir "${GPG_TEMP_DIR}" \
-    --no-tty \
-    --batch \
-    --armor \
-    --yes \
-    --local-user "${PGP_SIGN_ID}" \
-    --detach-sign \
-    --output "${RELEASE_FILE}.gpg" \
-    "${RELEASE_FILE}"
+gpg2 --armor \
+     --batch \
+     --detach-sign \
+     --homedir "${GPG_TEMP_DIR}" \
+     --local-user "${PGP_SIGN_ID}" \
+     --no-tty \
+     --output "${RELEASE_FILE}.gpg" \
+     --passphrase-fd 0 \
+     --quiet \
+     --yes \
+     "${RELEASE_FILE}"
 
 rm -rf "${GPG_TEMP_DIR}"
 
