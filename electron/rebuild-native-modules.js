40e589b620e17b62a60c6ae1082b356471c29bb5
Florian <florian.keller@wire.com>
2017-05-16 12:24:10 +0200

    Linting, add header

--- electron/rebuild-native-modules.js
+++ electron/rebuild-native-modules.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
-
 /*
-   This is elijahmanor's cross-var (v1.0.3) with a few modifications.
-   https://github.com/elijahmanor/cross-var
-*/
+ * Wire
+ * Copyright (C) 2017 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
 
 'use strict';
 
@@ -13,8 +26,8 @@ const arch = process.env.wire_target_arch ? process.env.wire_target_arch : proce
 
 const normalize = (args) => {
   return args.map((arg) => {
-    Object.keys(process.env).forEach(key => {
-      const regex = new RegExp(`\\$${key }|%${key }%`, 'i');
+    Object.keys(process.env).forEach((key) => {
+      const regex = new RegExp(`\\$${key}|%${key}%`, 'i');
       arg = arg.replace(regex, process.env[key]);
     });
     return arg;
