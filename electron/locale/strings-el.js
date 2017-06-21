3ec9f6ca7d4c2d0cc9aacee1ca2e32b144eb25a4
Panayiotis Lipiridis <lipiridis@gmail.com>
2017-06-21 09:55:55 +0200

    Greek translations

---
+++ electron/locale/strings-el.js
@@ -0,0 +1,71 @@
+'use strict';
+
+let string = {};
+
+
+string.aboutReleases = 'Κυκλοφορίες Εκδόσεων';
+string.aboutUpdate = 'Πως λειτουργούν οι ενημερώσεις στο Wire';
+string.aboutVersion = 'Έκδοση';
+string.aboutWebappVersion = 'Έκδοση του Wire για Web';
+
+string.menuConversation = 'Συνομιλία';
+string.menuStart = 'Έναρξη';
+string.menuEdit = 'Eπεξεργασία';
+string.menuWindow = 'Παράθυρο';
+string.menuView = 'Προβολή';
+string.menuHelp = 'Βοήθεια';
+string.menuStartup = 'Εκτέλεση του Wire κατά την εκκίνηση';
+string.menuLocale = 'Γλώσσα';
+string.menuAbout = 'Σχετικά με το Wire';
+string.menuSignOut = 'Αποσύνδεση';
+string.menuPing = 'Σκούντημα';
+string.menuCall = 'Κλήση';
+string.menuVideoCall = 'Βιντεοκλήση';
+string.menuPeople = 'Άτομα';
+string.menuAddPeople = 'Προσθήκη ατόμων...';
+string.menuArchive = 'Αρχειοθέτηση';
+string.menuUnarchive = 'Αναίρεση αρχειοθέτησης';
+string.menuMute = 'Σίγαση';
+string.menuUnmute = 'Αύξηση έντασης';
+string.menuLeave = 'Αποχώρηση';
+string.menuDelete = 'Διαγραφή';
+string.menuBlock = 'Αποκλεισμός';
+string.menuUndo = 'Αναίρεση';
+string.menuRedo = 'Ακύρωση αναίρεσης';
+string.menuCut = 'Αποκοπή';
+string.menuCopy = 'Αντιγραφή';
+string.menuPaste = 'Επικόλληση';
+string.menuSelectAll = 'Επιλογή Όλων';
+string.menuSpelling = 'Ορθογραφικός έλεγχος κατά την πληκτρολόγηση';
+string.menuMinimize = 'Ελαχιστοποίηση';
+string.menuClose = 'Κλείσιμο';
+string.menuNextConversation = 'Επόμενη συνομιλία';
+string.menuPreviousConversation = 'Προηγούμενη συνομιλία';
+string.menuFullScreen = 'Πλήρης Οθόνη';
+string.menuLegal = 'Περί Νόμου';
+string.menuPrivacy = 'Προστασία Προσωπικών Δεδομένων';
+string.menuLicense = 'Πληροφορίες άδειας χρήσης';
+string.menuSupport = 'Ιστοσελίδα υποστήριξης';
+string.menuWireURL = 'Η Ιστοσελίδα του Wire';
+string.menuPreferences = 'Προτιμήσεις';
+string.menuServices = 'Υπηρεσίες';
+string.menuHideWire = 'Απόκρυψη Wire';
+string.menuHideOthers = 'Απόκρυψη υπολοίπων';
+string.menuShowAll = 'Προβολή Όλων';
+string.menuSettings = 'Ρυθμίσεις';
+string.menuQuit = 'Έξοδος από το Wire';
+string.menuShowHide = 'Προβολή Μενού';
+string.menuSavePictureAs = 'Αποθήκευση Εικόνας Ως...';
+string.menuNoSuggestions = 'Καμία πρόταση';
+
+string.restartNeeded = 'Απαιτείται επανεκκίνηση';
+string.restartLocale = 'Παρακαλούμε επανεκκινήστε την εφαρμογή για να διατηρήσετε τις τρέχουσες ρυθμίσεις.';
+string.restartLater = 'Αργότερα';
+string.restartNow = 'Επανεκκίνηση τώρα';
+
+string.trayOpen = 'Άνοιγμα';
+string.trayQuit = 'Έξοδος';
+
+string.unreadMessages = 'Μη αναγνωσμένο μήνυμα';
+
+module.exports = string;
