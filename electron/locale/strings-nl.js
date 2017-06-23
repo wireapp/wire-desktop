d727138c3551f9b687c73fa69b6bfbfc5c17d8c9
Gregor Herdmann <gregor@wire.com>
2017-06-23 16:27:27 +0200

    Add Dutch and Polish

---
+++ electron/locale/strings-nl.js
@@ -0,0 +1,71 @@
+'use strict';
+
+let string = {};
+
+
+string.aboutReleases = 'Releases';
+string.aboutUpdate = 'Hoe Werken Wire Updates';
+string.aboutVersion = 'Versie';
+string.aboutWebappVersion = 'Wire voor Web Versie';
+
+string.menuConversation = 'Gesprek';
+string.menuStart = 'Beginnen';
+string.menuEdit = 'Bewerken';
+string.menuWindow = 'Venster';
+string.menuView = 'Toon';
+string.menuHelp = 'Help';
+string.menuStartup = 'Start Wire bij Opstarten';
+string.menuLocale = 'Taal';
+string.menuAbout = 'Over Wire';
+string.menuSignOut = 'Afmelden';
+string.menuPing = 'Ping';
+string.menuCall = 'Oproep';
+string.menuVideoCall = 'Video-oproep';
+string.menuPeople = 'Deelnemers';
+string.menuAddPeople = 'Deelnemers toevoegen...';
+string.menuArchive = 'Archiveren';
+string.menuUnarchive = 'Terugzetten';
+string.menuMute = 'Dempen';
+string.menuUnmute = 'Dempen opheffen';
+string.menuLeave = 'Verlaten';
+string.menuDelete = 'Verwijderen';
+string.menuBlock = 'Blokkeren';
+string.menuUndo = 'Ongedaan maken';
+string.menuRedo = 'Opnieuw';
+string.menuCut = 'Knippen';
+string.menuCopy = 'Kopiëren';
+string.menuPaste = 'Plakken';
+string.menuSelectAll = 'Alles selecteren';
+string.menuSpelling = 'Controleer op Spelling Tijdens Typen';
+string.menuMinimize = 'Minimaliseren';
+string.menuClose = 'Sluiten';
+string.menuNextConversation = 'Volgende gesprek';
+string.menuPreviousConversation = 'Vorige gesprek';
+string.menuFullScreen = 'Volledig scherm';
+string.menuLegal = 'Voorwaarden';
+string.menuPrivacy = 'Privacy';
+string.menuLicense = 'Licentie-informatie';
+string.menuSupport = 'Support Website';
+string.menuWireURL = 'Wire Website';
+string.menuPreferences = 'Voorkeuren';
+string.menuServices = 'Services';
+string.menuHideWire = 'Wire verbergen';
+string.menuHideOthers = 'Verberg anderen';
+string.menuShowAll = 'Alles tonen';
+string.menuSettings = 'Instellingen';
+string.menuQuit = 'Wire afsluiten';
+string.menuShowHide = 'Toon Menu';
+string.menuSavePictureAs = 'Afbeelding opslaan als...';
+string.menuNoSuggestions = 'Geen suggesties';
+
+string.restartNeeded = 'Herstart nodig';
+string.restartLocale = 'Start de app opnieuw op om deze instelling toe te passen.';
+string.restartLater = 'Later';
+string.restartNow = 'Herstart nu';
+
+string.trayOpen = 'Openen';
+string.trayQuit = 'Sluit';
+
+string.unreadMessages = 'Ongelezen bericht';
+
+module.exports = string;
