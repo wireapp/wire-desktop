10986e553bc25f60a918ab23a04f509cb512bc8d
herrmannplatz <rene@herrmannplatz.de>
2017-06-26 20:44:15 +0200

    fix badge count

--- electron/renderer/src/lib/badgeCount.js
+++ electron/renderer/src/lib/badgeCount.js
@@ -2,7 +2,7 @@ export default function(title) {
   const matches = (/\(([0-9]+)\)/).exec(title)
 
   if (matches !== null) {
-    return parseInt(counter[1], 10)
+    return parseInt(matches[1], 10)
   }
   return undefined
 }
