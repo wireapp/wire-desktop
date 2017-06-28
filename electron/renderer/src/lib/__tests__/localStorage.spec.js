c210a19713d3af1dccd6147df4febac48570ac9a
herrmannplatz <rene@herrmannplatz.de>
2017-06-28 07:54:56 +0200

    add test setup

---
+++ electron/renderer/src/lib/__tests__/localStorage.spec.js
@@ -0,0 +1,47 @@
+/*
+ * Wire
+ * Copyright (C) 2017 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import { saveState, loadState } from '../localStorage'
+
+import LocalStorageMock from '../__mocks__/localStorage';
+
+
+describe('localStorage', () => {
+
+  beforeEach(() => {
+    global.localStorage = new LocalStorageMock();
+  })
+
+  afterEach(() => {
+    delete global.localStorage
+  })
+
+  it('should return saved state', () => {
+    const state = {
+      foo: 'string',
+      bar: true,
+      num: 1,
+      test: null
+    }
+
+    saveState(state)
+
+    expect(loadState()).toEqual(state)
+  })
+})
