3c4bd08988687f554280a4c293ff2b6d1026ba86
rehez <rene@wearezeta.com>
2017-07-06 10:47:52 +0200

    lint actions folder

--- electron/renderer/src/actions/index.js
+++ electron/renderer/src/actions/index.js
@@ -1,64 +1,64 @@
-import uuid from 'uuid/v4'
-import verifyObjectProperties from '../lib/verifyObjectProperties'
+import uuid from 'uuid/v4';
+import verifyObjectProperties from '../lib/verifyObjectProperties';
 
-export const ADD_ACCOUNT = 'ADD_ACCOUNT'
-export const SWITCH_ACCOUNT = 'SWITCH_ACCOUNT'
-export const UPDATE_ACCOUNT = 'UPDATE_ACCOUNT'
-export const UPDATE_ACCOUNT_BADGE = 'UPDATE_ACCOUNT_BADGE'
-export const DELETE_ACCOUNT = 'DELETE_ACCOUNT'
+export const ADD_ACCOUNT = 'ADD_ACCOUNT';
+export const SWITCH_ACCOUNT = 'SWITCH_ACCOUNT';
+export const UPDATE_ACCOUNT = 'UPDATE_ACCOUNT';
+export const UPDATE_ACCOUNT_BADGE = 'UPDATE_ACCOUNT_BADGE';
+export const DELETE_ACCOUNT = 'DELETE_ACCOUNT';
 
 export const addAccountWithSession = () => {
   return {
     type: ADD_ACCOUNT,
-    sessionID: uuid()
-  }
-}
+    sessionID: uuid(),
+  };
+};
 
 export const updateAccount = (id, data) => {
   return {
     type: UPDATE_ACCOUNT,
     id,
-    data
-  }
-}
+    data,
+  };
+};
 
 export const addAccount = () => {
   return {
-    type: ADD_ACCOUNT
-  }
-}
+    type: ADD_ACCOUNT,
+  };
+};
 
 export const switchAccount = id => {
   return {
     type: SWITCH_ACCOUNT,
-    id
-  }
-}
+    id,
+  };
+};
 
 export const updateAccountBadge = (id, count) => {
   return {
     type: UPDATE_ACCOUNT_BADGE,
     id,
-    count
-  }
-}
+    count,
+  };
+};
 
 export const deleteAccount = id => {
   return {
     type: DELETE_ACCOUNT,
-    id
-  }
-}
+    id,
+  };
+};
 
 export const abortAccountCreation = (id) => {
   return (dispatch, getState) => {
-    dispatch(deleteAccount(id))
+    dispatch(deleteAccount(id));
 
-    const accounts = getState().accounts
-    const lastAccount = accounts[accounts.length - 1]
-    dispatch(switchAccount(lastAccount.id))
-  }
-}
+    const accounts = getState().accounts;
+    const lastAccount = accounts[accounts.length - 1];
+    dispatch(switchAccount(lastAccount.id));
+  };
+};
 export const updateAccountData = (id, data) => {
   return (dispatch, getState) => {
     const isValidAccountData = verifyObjectProperties(data, {
@@ -66,13 +66,13 @@ export const updateAccountData = (id, data) => {
       'userID': 'String',
       'picture': 'String',
       'name': 'String',
-      'accentID': 'Number'
-    })
+      'accentID': 'Number',
+    });
 
     if (isValidAccountData) {
-      dispatch(updateAccount(id, data))
+      dispatch(updateAccount(id, data));
     } else {
-      console.warn(`Got invalid account data ${JSON.stringify(data)}`)
+      console.warn(`Got invalid account data ${JSON.stringify(data)}`);
     }
-  }
-}
+  };
+};
