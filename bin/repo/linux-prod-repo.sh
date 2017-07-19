2df45bc733e89b39d9083f18113145aaeff7a1d1
Sven Jost <sven.jost@googlemail.com>
2017-07-19 18:22:27 +0200

    Make script compatible to gpgv2 2.1.x

--- bin/repo/linux-prod-repo.sh
+++ bin/repo/linux-prod-repo.sh
@@ -37,11 +37,6 @@ mkdir -p "${BINARY_DIR}" "${STABLE_DIR}/main/binary-"{all,i386,amd64} "${CONF_DI
 
 if ! _command_exist "gpg2"; then
   _error_exit "Could not find gpg2. Please install package 'gnupg2' version 2.0.x."
-else
-  GPG_VERSION="$(gpg2 --version | head -n 1 | grep -oE '2\..*')"
-#  if ! grep -qE "2\\.0" <<< "${GPG_VERSION}"; then
-#    _error_exit "Your gpg2 version is ${GPG_VERSION}. Signing with a predefined passphrase will work only with gpg2 version 2.0.x."
-#  fi
 fi
 
 if ! _command_exist "apt-ftparchive"; then
@@ -94,6 +89,10 @@ gpg2 --batch \
      --quiet \
      --import "${PGP_KEYFILE}"
 
+_log "Update gpg2 configuration to sign on unattended machines..."
+echo "allow-loopback-pinentry" > ~/.gnupg/gpg-agent.conf
+killall gpg-agent
+
 echo "${PGP_PASSPHRASE}" | \
 gpg2 --armor \
      --batch \
@@ -102,6 +101,7 @@ gpg2 --armor \
      --local-user "${PGP_SIGN_ID}" \
      --no-tty \
      --output "${RELEASE_FILE}.gpg" \
+     --pinentry-mode loopback \
      --passphrase-fd 0 \
      --quiet \
      --yes \
@@ -114,6 +114,7 @@ gpg2 --batch \
      --local-user "${PGP_SIGN_ID}" \
      --no-tty \
      --output "${INRELEASE_FILE}" \
+     --pinentry-mode loopback \
      --passphrase-fd 0 \
      --quiet \
      --yes \
