56caa27391092ccd7ce6d710b24a5f5f1f105c68
herrmannplatz <rene@herrmannplatz.de>
2017-07-19 08:17:33 +0200

    add test folders

---
+++ electron/js/lib/__tests__/openGraph.spec.js
@@ -0,0 +1,65 @@
+/*
+ * Wire
+ * Copyright (C) 2017 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import nock from 'nock'
+
+import openGraph from '../openGraph';
+
+describe('openGraph', () => {
+
+  it('should return open graph data with image data', (done) => {
+    const openGraphData = {
+      "title": "Wire · Modern communication, full privacy. For iOS, Android, macOS, Windows, Linux and web.",
+      "type": "website",
+      "url": "https://wire.com/",
+      "image": {
+        "url": "https://lh3.ggpht.com/ElqTCcY1N0c3EAX27MRFoXynZlbTaJD2KEqYNXAPn5YQPZa6Bvsux4NCgEMoUhazdIWWelAU__Kzmr55j55EsgM=s1024"
+      },
+      "description": "HD quality calls, private and group chats with inline photos, music and video. Secure and perfectly synced across your devices."
+    };
+
+    openGraph('https://wire.com/', (error, meta) => {
+      expect(error).toBeNull();
+      expect(meta.title).toBe(openGraphData.title);
+      expect(meta.image.data).toBeDefined();
+      done();
+    });
+
+  });
+
+  it('should return open graph data without image data', (done) => {
+    const openGraphData = {
+      "title": "Wire · Modern communication, full privacy. For iOS, Android, macOS, Windows, Linux and web.",
+      "type": "website",
+      "url": "https://wire.com/",
+      "description": "HD quality calls, private and group chats with inline photos, music and video. Secure and perfectly synced across your devices."
+    };
+
+    openGraph('https://wire.com/', (error, meta) => {
+      expect(error).toBeNull();
+      expect(meta.title).toBe(openGraphData.title);
+      expect(meta.image).not.toBeDefined();
+      done();
+    });
+
+  });
+
+});
+
+
