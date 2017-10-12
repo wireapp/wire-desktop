bebc917277aa137d1f44cf02f5dbd33ddbea9743
Gregor Herdmann <gregor@wire.com>
2017-10-12 11:48:45 +0200

    feat: Add Estonian translations (#1031)

---
+++ electron/locale/strings-et.js
@@ -0,0 +1,75 @@
+'use strict';
+
+let string = {};
+
+
+
+string.aboutReleases = 'Väljalasked';
+string.aboutUpdate = 'Kuidas Wire’i uuendused töötavad';
+string.aboutVersion = 'Versioon';
+string.aboutWebappVersion = 'Wire for Web versioon';
+
+string.menuConversation = 'Vestlus';
+string.menuStart = 'Alusta';
+string.menuEdit = 'Muuda';
+string.menuWindow = 'Aken';
+string.menuView = 'Vaade';
+string.menuHelp = 'Abi';
+string.menuStartup = 'Käivita Wire süsteemi käivitusel';
+string.menuLocale = 'Keel';
+string.menuAbout = 'Teave Wire kohta';
+string.menuSignOut = 'Logi välja';
+string.menuPing = 'Ping';
+string.menuCall = 'Kõne';
+string.menuVideoCall = 'Videokõne';
+string.menuPeople = 'Inimesed';
+string.menuAddPeople = 'Lisa inimesi...';
+string.menuArchive = 'Arhiveeri';
+string.menuUnarchive = 'Arhiivist tagasi';
+string.menuMute = 'Vaigista';
+string.menuUnmute = 'Lõpeta vaigistus';
+string.menuLeave = 'Lahku';
+string.menuDelete = 'Kustuta';
+string.menuBlock = 'Blokeeri';
+string.menuUndo = 'Võta tagasi';
+string.menuRedo = 'Tee uuesti';
+string.menuCut = 'Lõika';
+string.menuCopy = 'Kopeeri';
+string.menuPaste = 'Kleebi';
+string.menuSelectAll = 'Vali kõik';
+string.menuSpelling = 'Kontrolli õigekirja kirjutamise ajal';
+string.menuMinimize = 'Minimeeri';
+string.menuClose = 'Sulge';
+string.menuNextConversation = 'Järgmine vestlus';
+string.menuPreviousConversation = 'Eelmine vestlus';
+string.menuFullScreen = 'Täisekraan';
+string.menuLegal = 'Juriidiline info';
+string.menuPrivacy = 'Privaatsus';
+string.menuLicense = 'Litsentsiteave';
+string.menuSupport = 'Kasutajatoe veebisait';
+string.menuWireURL = 'Wire’i koduleht';
+string.menuPreferences = 'Eelistused';
+string.menuServices = 'Teenused';
+string.menuHideWire = 'Peida Wire';
+string.menuHideOthers = 'Peida teised';
+string.menuShowAll = 'Kuva kõik';
+string.menuSettings = 'Seaded';
+string.menuQuit = 'Sulge Wire';
+string.menuShowHide = 'Kuva menüü';
+string.menuSavePictureAs = 'Salvesta pilt nimega...';
+string.menuNoSuggestions = 'Soovitused puuduvad';
+
+string.restartNeeded = 'Taaskäivitamine vajalik';
+string.restartLocale = 'Palun taaskäivita rakendus selle sätte jõustumiseks.';
+string.restartLater = 'Hiljem';
+string.restartNow = 'Taaskäivita kohe';
+
+string.trayOpen = 'Ava';
+string.trayQuit = 'Välju';
+
+string.unreadMessages = 'Lugemata sõnum';
+
+string.wrapperAddAccount = 'Lisa konto';
+string.wrapperCreateTeam = 'Loo tiimikonto';
+
+module.exports = string;
