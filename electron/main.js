25a3b5ddd3571be2cee684aaef04d309e03267fa
Tim <AtomicCons@users.noreply.github.com>
2018-01-09 04:35:27 -0700

    feat: PoP!_OS/Gnome Indicator (#1187)

--- electron/main.js
+++ electron/main.js
@@ -134,8 +134,9 @@ if (environment.platform.IS_WINDOWS) {
 ///////////////////////////////////////////////////////////////////////////////
 if (environment.platform.IS_LINUX) {
   const isUbuntuUnity = process.env.XDG_CURRENT_DESKTOP && process.env.XDG_CURRENT_DESKTOP.includes('Unity');
-
-  if (isUbuntuUnity) {
+  const isPopOS = process.env.XDG_CURRENT_DESKTOP && process.env.XDG_CURRENT_DESKTOP.includes('pop');
+  const isGnome = process.env.XDG_CURRENT_DESKTOP && process.env.XDG_CURRENT_DESKTOP.includes('GNOME');
+  if (isUbuntuUnity || isPopOS || isGnome) {
     process.env.XDG_CURRENT_DESKTOP = 'Unity';
   }
 }
