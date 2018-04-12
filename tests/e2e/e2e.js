7b6e6f306221219d142d18a6ac7eaa281deb106d
Thomas Belin <me@thomasbelin.fr>
2018-04-12 17:12:09 +0200

    Force logging of content of page on CI

--- tests/e2e/e2e.js
+++ tests/e2e/e2e.js
@@ -1,10 +1,9 @@
-const Application = require('spectron').Application
-const assert = require('assert')
+const Application = require('spectron').Application;
+const assert = require('assert');
 const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
-const path = require('path')
 
 describe('Application launch', function () {
-  this.timeout(10000)
+  this.timeout(10000);
 
   beforeEach(function () {
     this.app = new Application({
@@ -26,7 +25,7 @@ describe('Application launch', function () {
 
       // The following line tells spectron to look and use the main.js file
       // and the package.json located 1 level above.
-      args: ["electron/"]
+      args: ['electron/']
     });
     return this.app.start();
   });
@@ -37,14 +36,11 @@ describe('Application launch', function () {
     if (this.app && this.app.isRunning()) {
       return this.app.stop();
     }
-  })
+  });
 
   it('shows an initial window', function () {
-    return this.app.client.getWindowCount().then(function (count) {
-      console.log(count);
-      assert.equal(count, 1)
-      // Please note that getWindowCount() will return 2 if `dev tools` are opened.
-      // assert.equal(count, 2)
-    })
-  })
+    return this.app.client
+      .getHTML('html').then(console.log.bind(console));
+    //return this.app.client.getTitle().then(console.log.bind(console));
+  });
 });
