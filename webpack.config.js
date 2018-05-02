893cd314240e9783a363887999f3ea5775df0468
Florian Keller <florian.keller@wire.com>
2018-05-02 13:02:52 +0200

    Simplify other switches

--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,32 @@ const webpack = require('webpack');
 
 module.exports = (env = {}) => {
   return {
+    devtool: env.production ? '' : 'cheap-eval-source-map',
     entry: path.resolve(__dirname, 'electron/renderer/src/index.js'),
     output: {
       path: path.resolve(__dirname, 'electron/renderer/dist'),
-      filename: 'bundle.js'
+      filename: 'bundle.js',
     },
+    mode: env.production ? 'production' : 'development',
     module: {
       rules: [
         {
-          test: /\.js$/,
           exclude: /(node_modules)/,
-          use: ['babel-loader']
+          test: /\.js$/,
+          use: ['babel-loader'],
         },
         {
           test: /\.css$/,
-          use: [ 'style-loader', 'css-loader' ]
-        }
-      ]
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
     },
-    mode: env.production ? 'production' : 'development',
-    devtool: (() => {
-      if (!env.production) {
-        return 'cheap-eval-source-map'
-      }
-    })(),
-    plugins: (() => {
-      if (env.production) {
-        return [
+    plugins: env.production
+      ? [
           new webpack.DefinePlugin({
-            'process.env': {
-              'NODE_ENV': JSON.stringify('production')
-            }
-          })
-        ];
-      }
-    })()
+            'process.env': {NODE_ENV: JSON.stringify('production')},
+          }),
+        ]
+      : [],
   };
-}
+};
