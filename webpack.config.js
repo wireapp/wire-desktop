a81e50817a344aa29e807fe20503d6cbcf8698be
Lipis <lipiridis@gmail.com>
2018-05-02 13:49:18 +0200

    Proper linting (#1058)

--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,39 @@ const webpack = require('webpack');
 
 module.exports = (env = {}) => {
   return {
+    devtool: (() => {
+      if (!env.production) {
+        return 'cheap-eval-source-map';
+      }
+    })(),
     entry: path.resolve(__dirname, 'electron/renderer/src/index.js'),
-    output: {
-      path: path.resolve(__dirname, 'electron/renderer/dist'),
-      filename: 'bundle.js'
-    },
     module: {
       rules: [
         {
-          test: /\.js$/,
           exclude: /(node_modules)/,
-          use: ['babel-loader']
+          test: /\.js$/,
+          use: ['babel-loader'],
         },
         {
           test: /\.css$/,
-          use: [ 'style-loader', 'css-loader' ]
-        }
-      ]
+          use: [ 'style-loader', 'css-loader' ],
+        },
+      ],
     },
-    devtool: (() => {
-      if (!env.production) {
-        return 'cheap-eval-source-map'
-      }
-    })(),
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'electron/renderer/dist'),
+    },
     plugins: (() => {
       if (env.production) {
         return [
           new webpack.DefinePlugin({
             'process.env': {
-              'NODE_ENV': JSON.stringify('production')
-            }
-          })
+              'NODE_ENV': JSON.stringify('production'),
+            },
+          }),
         ];
       }
-    })()
+    })(),
   };
-}
+};
