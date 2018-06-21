c9c00fbaca2dc0b1bfdb21183e277206ba824ac4
Gregor Herdmann <gregor@wire.com>
2018-06-21 12:32:30 +0200

    fix: Add path import for portable flag

--- electron/js/appInit.js
+++ electron/js/appInit.js
@@ -20,6 +20,7 @@
 const {app} = require('electron');
 const environment = require('./environment');
 const minimist = require('minimist');
+const path = require('path');
 
 const argv = minimist(process.argv.slice(1));
 
