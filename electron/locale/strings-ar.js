0d2f4b2c73b27c0609a668f55ce2e9be227b8a78
Youssif Shaaban Alsager <ysh-alsager@hotmail.com>
2018-08-04 17:58:20 +0200

    Add Arabic translation [1/2]

---
+++ electron/locale/strings-ar.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const string = {};
+
+string.aboutReleases = 'اﻹصدارات';
+string.aboutUpdate = 'كيف تعمل تحديثات واير';
+string.aboutVersion = 'اﻹصدار';
+string.aboutWebappVersion = 'إصدار واير نسخة الويب';
+
+string.menuConversation = 'المحادثات';
+string.menuStart = 'محادثة جديدة';
+string.menuEdit = 'تحرير';
+string.menuWindow = 'النوافذ';
+string.menuView = 'عرض';
+string.menuHelp = 'مساعدة';
+string.menuStartup = 'تشغيل واير مع بدء التشغيل';
+string.menuLocale = 'اللغة';
+string.menuAbout = 'حول واير';
+string.menuSignOut = 'تسجيل الخروج';
+string.menuPing = 'رنين';
+string.menuCall = 'مكالمة';
+string.menuVideoCall = 'مكالمة فيديو';
+string.menuPeople = 'اﻷشخاص';
+string.menuAddPeople = 'إضافة أشخاص...';
+string.menuArchive = 'أرشفة';
+string.menuUnarchive = 'إلغاء اﻷرشفة';
+string.menuMute = 'كتم';
+string.menuUnmute = 'إلغاء الكتم';
+string.menuLeave = 'مغادرة';
+string.menuDelete = 'حذف المحتوي...';
+string.menuBlock = 'حظر';
+string.menuUndo = 'تراجع';
+string.menuRedo = 'إعادة';
+string.menuCut = 'قص';
+string.menuCopy = 'نسخ';
+string.menuPaste = 'لصق';
+string.menuSelectAll = 'تحديد الكل';
+string.menuSpelling = 'تدقيق إملائي أثناء الكتابة';
+string.menuMinimize = 'تصغير';
+string.menuClose = 'إغلاق';
+string.menuNextConversation = 'المحادثة التالية';
+string.menuPreviousConversation = 'المحادثة السابقة';
+string.menuFullScreen = 'شاشة كاملة';
+string.menuLegal = 'المعلومات القانونية';
+string.menuPrivacy = 'الخصوصية';
+string.menuLicense = 'معلومات الترخيص';
+string.menuSupport = 'موقع الدعم';
+string.menuWireURL = 'موقع واير الالكتروني';
+string.menuPreferences = 'التفضيلات';
+string.menuServices = 'الخدمات';
+string.menuHideWire = 'إخفاء واير';
+string.menuHideOthers = 'إخفاء اﻷخرين';
+string.menuShowAll = 'عرض الكل';
+string.menuSettings = 'اﻹعدادات';
+string.menuQuit = 'مغادرة واير';
+string.menuShowHide = 'عرض القائمة';
+string.menuSavePictureAs = 'حفظ الصورة كـ...';
+string.menuNoSuggestions = 'لا توجد اقتراحات';
+
+string.restartNeeded = 'إعادة التشغيل مطلوبة';
+string.restartLocale = 'الرجاء إعادة تشغيل التطبيق ليتم تفعيل هذا الإعداد.';
+string.restartLater = 'لاحقا';
+string.restartNow = 'إعادة التشغيل اﻵن';
+
+string.trayOpen = 'فتح';
+string.trayQuit = 'إغلاق';
+
+string.unreadMessages = 'رسالة غير مقروئة';
+
+string.wrapperAddAccount = 'إضافة حساب';
+string.wrapperCreateTeam = 'إنشاء فريق';
+string.wrapperLogOut = 'تسجيل الخروج';
+string.wrapperManageTeam = 'إدارة الفريق';
+string.wrapperRemoveAccount = 'حذف الحساب';
+
+module.exports = string;
