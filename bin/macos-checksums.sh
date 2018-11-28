cfffb616504f8836ad68f17cd5eab7da43b56112
Florian Keller <florian.keller@wire.com>
2018-11-28 15:34:13 +0100

    use gpg instead of gpg2

--- bin/macos-checksums.sh
+++ bin/macos-checksums.sh
@@ -91,17 +91,17 @@ gpg --batch \
 _log "Signing source code archive with PGP key..."
 
 echo "${PGP_PASSPHRASE}" | \
-gpg2 --batch \
-     --clearsign \
-     --homedir "${GPG_TEMP_DIR}" \
-     --local-user "${PGP_SIGN_ID}" \
-     --no-tty \
-     --output "${BUILD_VERSION}.tar.gz.sig" \
-     --pinentry-mode loopback \
-     --passphrase-fd 0 \
-     --quiet \
-     --yes \
-     "${BUILD_VERSION}.tar.gz"
+gpg --batch \
+    --clearsign \
+    --homedir "${GPG_TEMP_DIR}" \
+    --local-user "${PGP_SIGN_ID}" \
+    --no-tty \
+    --output "${BUILD_VERSION}.tar.gz.sig" \
+    --pinentry-mode loopback \
+    --passphrase-fd 0 \
+    --quiet \
+    --yes \
+    "${BUILD_VERSION}.tar.gz"
 
 rm "${BUILD_VERSION}.tar.gz"
 
