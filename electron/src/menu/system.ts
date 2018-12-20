43912f8da4c1c71b9e07a7b159386b782581beee
Florian Keller <florian@wire.com>
2018-12-20 11:34:27 +0100

    fix: Don't scroll on PageUp (Linux)

--- electron/src/menu/system.ts
+++ electron/src/menu/system.ts
@@ -454,6 +454,10 @@ const registerShortcuts = (): void => {
   // Global mute shortcut
   globalShortcut.register('CmdOrCtrl+Alt+M', () => sendAction(EVENT_TYPE.CONVERSATION.TOGGLE_MUTE));
 
+  if (environment.platform.IS_LINUX) {
+    globalShortcut.register('PageUp', () => void 0);
+  }
+
   // Global account switching shortcut
   const switchAccountShortcut = ['CmdOrCtrl', 'Super'];
   const accountLimit = 3;
