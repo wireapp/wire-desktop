bbf33fd4fc40398101d479c3b08689b8223b558c
Maxim Baz <git@maximbaz.com>
2018-12-28 19:08:39 +0100

    Create a detached signature for the sources archive

--- bin/linux-checksums.sh
+++ bin/linux-checksums.sh
@@ -111,11 +111,10 @@ _log "Signing source code archive with PGP key..."
 
 echo "${PGP_PASSPHRASE}" | \
 gpg2 --batch \
-     --clearsign \
+     --detach-sign \
      --homedir "${GPG_TEMP_DIR}" \
      --local-user "${PGP_SIGN_ID}" \
      --no-tty \
-     --output "${BUILD_VERSION}.tar.gz.sig" \
      --pinentry-mode loopback \
      --passphrase-fd 0 \
      --quiet \
