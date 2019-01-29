c61b0c845edb56f9cf29d66523df834d282a412b
Florian Keller <florian.keller@wire.com>
2019-01-29 12:00:28 +0100

    chore: Use @wireapp/copy-config

---
+++ .copyconfigrc.js
@@ -0,0 +1,15 @@
+const pkg = require('./package.json');
+const path = require('path');
+
+const logoSource = path.join(pkg.name, 'content', 'image', 'logo');
+const repositoryUrl = pkg.dependencies['wire-web-config-default'];
+
+module.exports = {
+  files: {
+    [path.join(logoSource, '32x32.png')]: 'resources/icons/32x32.png',
+    [path.join(logoSource, '256x256.png')]: ['resources/icons/256x256.png', 'electron/img/wire.256.png'],
+    [path.join(logoSource, 'wire.icns')]: 'resources/macos/wire.icns',
+    [path.join(logoSource, 'wire.ico')]: 'electron/img/wire.ico',
+  },
+  repositoryUrl,
+}
