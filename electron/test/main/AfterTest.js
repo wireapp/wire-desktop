2bb1440bb8fcf8007431e77d71f355d8ffb83630
Benny Neugebauer <bn@bennyn.de>
2019-02-15 15:07:08 +0100

    Seeing light at the end of the tunnel

--- electron/test/main/AfterTest.js
+++ electron/test/main/AfterTest.js
@@ -5,10 +5,14 @@ const fs = require('fs-extra');
  * Bind to mocha's "after" hook in order to write coverage info:
  * https://github.com/jprichardson/electron-mocha/issues/19#issuecomment-193451731
  */
+const writeCoverageReport = coverage => {
+  const outputFile = path.resolve(process.cwd(), 'instrumented-code/coverage.json');
+  fs.writeJsonSync(outputFile, coverage);
+};
+
 after(() => {
   const info = global.__coverage__;
   if (info) {
-    const outputFile = path.resolve(process.cwd(), 'instrumented-code/coverage.json');
-    fs.writeJsonSync(outputFile, info);
+    writeCoverageReport(info);
   }
 });
