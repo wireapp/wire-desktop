84c69d800b044c2bf48c7d03c5dfe1ce1cf697c3
Florian Keller <florian.keller@wire.com>
2019-02-26 14:32:04 +0100

    rename wire.256.png

--- .copyconfigrc.js
+++ .copyconfigrc.js
@@ -9,7 +9,7 @@ const repositoryUrl = pkg.dependencies['wire-web-config-default'];
 module.exports = {
   files: {
     [path.join(logoSource, '32x32.png')]: 'resources/icons/32x32.png',
-    [path.join(logoSource, '256x256.png')]: ['resources/icons/256x256.png', 'electron/img/wire.256.png'],
+    [path.join(logoSource, '256x256.png')]: ['resources/icons/256x256.png', 'electron/img/logo.256.png'],
     [path.join(logoSource, 'logo.ico')]: 'electron/img/logo.ico',
     [path.join(macOsSource, '**')]: 'resources/macos/',
     [path.join(pkg.name, '.env.defaults')]: '.env.defaults',
