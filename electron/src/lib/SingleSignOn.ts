c85d3b35abea3f739ec4e7c9a34b8ddb18d73ead
Florian Keller <florian.keller@wire.com>
2019-02-28 13:50:29 +0100

    add await

--- electron/src/lib/SingleSignOn.ts
+++ electron/src/lib/SingleSignOn.ts
@@ -227,7 +227,7 @@ class SingleSignOn {
     await SingleSignOn.protocol.register(this.session, (type: string) => this.finalizeLogin(type));
 
     // Show the window(s)
-    SingleSignOnLoginWindow.loadURL(this.windowOriginUrl.toString());
+    await SingleSignOnLoginWindow.loadURL(this.windowOriginUrl.toString());
 
     if (argv.devtools) {
       SingleSignOnLoginWindow.webContents.openDevTools({mode: 'detach'});
