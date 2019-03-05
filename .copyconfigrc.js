921b706402da80077ded3bdcefe1594ad1c633a0
Florian Keller <ffflorian@users.noreply.github.com>
2019-03-05 17:00:30 +0100

    chore: Load tray icons from config repo (#2288)

--- .copyconfigrc.js
+++ .copyconfigrc.js
@@ -1,19 +1,17 @@
 const pkg = require('./package.json');
-const path = require('path');
 
-const contentSource = path.join(pkg.name, 'content');
-const logoSource = path.join(contentSource, 'image/logo');
-const macOsSource = path.join(contentSource, 'macos');
+const contentSource = `${pkg.name}/content`;
+const imageSource = `${contentSource}/image`;
+const macOsSource = `${contentSource}/macos`;
 
 const configurationEntry = `wire-web-config-default-${process.env.BUILD_ENV !== 'internal' ? 'production' : 'internal'}`;
 const repositoryUrl = pkg.dependencies[configurationEntry];
 
 module.exports = {
   files: {
-    [`${logoSource}/32x32.png`]: 'resources/icons/32x32.png',
-    [`${logoSource}/256x256.png`]: ['resources/icons/256x256.png', 'electron/img/logo.256.png'],
-    [`${logoSource}/logo.ico`]: 'electron/img/logo.ico',
+    [`${imageSource}/**`]: 'electron/img/',
     [`${macOsSource}/**`]: 'resources/macos/',
+    [`${imageSource}/logo/256x256.png`]: ['resources/icons/256x256.png', 'electron/img/logo.256.png'],
     [`${pkg.name}/.env.defaults`]: '.env.defaults',
   },
   repositoryUrl,
