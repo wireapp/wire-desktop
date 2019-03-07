12deb43a6488baad86d9cf02e799c87a80150ddb
Florian Keller <florian.keller@wire.com>
2019-03-07 17:32:32 +0100

    fix: Copy logo to resources

--- .copyconfigrc.js
+++ .copyconfigrc.js
@@ -11,7 +11,8 @@ module.exports = {
   files: {
     [`${imageSource}/**`]: 'electron/img/',
     [`${macOsSource}/**`]: 'resources/macos/',
-    [`${imageSource}/logo/256x256.png`]: 'electron/img/logo.256.png',
+    [`${imageSource}/logo/256x256.png`]: ['resources/icons/256x256.png', 'electron/img/logo.256.png'],
+    [`${imageSource}/logo/32x32.png`]: 'resources/icons/32x32.png',
     [`${imageSource}/logo/logo.ico`]: 'electron/img/logo.ico',
     [`${pkg.name}/.env.defaults`]: '.env.defaults',
   },
