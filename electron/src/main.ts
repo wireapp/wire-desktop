06f9305581c9fb904dabfc60870e0020c44dac09
Benny Neugebauer <bn@bennyn.de>
2019-03-12 11:54:59 +0100

    runfix: Handle invalid URLs when getting webview ID

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -318,8 +318,13 @@ const initElectronLogFile = (): void => {
 };
 
 const getWebViewId = (contents: Electron.WebContents): string | null => {
-  const currentLocation = new URL(contents.getURL());
-  return currentLocation.searchParams.get('id');
+  try {
+    const currentLocation = new URL(contents.getURL());
+    const webViewId = currentLocation.searchParams.get('id');
+    return webViewId && ValidationUtil.isUUIDv4(webViewId) ? webViewId : null;
+  } catch (error) {
+    return null;
+  }
 };
 
 class ElectronWrapperInit {
@@ -396,7 +401,7 @@ class ElectronWrapperInit {
           if (ENABLE_LOGGING) {
             contents.on('console-message', async (event, level, message) => {
               const webViewId = getWebViewId(contents);
-              if (webViewId && ValidationUtil.isUUIDv4(webViewId)) {
+              if (webViewId) {
                 const logFilePath = path.join(app.getPath('userData'), 'logs', webViewId, config.LOG_FILE_NAME);
                 try {
                   await LogFactory.writeMessage(message, logFilePath);
