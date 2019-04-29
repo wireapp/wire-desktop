ff26f20747dffcf4e66e2c88d383a95e23874679
Florian Keller <florian.keller@wire.com>
2019-04-29 11:52:26 +0200

    tray.icons is available

--- electron/src/menu/TrayHandler.test.main.ts
+++ electron/src/menu/TrayHandler.test.main.ts
@@ -30,7 +30,7 @@ describe('initTray', () => {
   it('creates native images for all tray icons and sets a default tray icon', () => {
     const tray = new TrayHandler();
     tray.initTray(TrayMock);
-    assert.strictEqual(Object.keys(tray.icons || {}).length, 3);
+    assert.strictEqual(Object.keys(tray.icons!).length, 3);
     assert.strictEqual(tray.icons!.badge.constructor.name, 'NativeImage');
     assert.strictEqual(tray.icons!.tray.constructor.name, 'NativeImage');
     assert.strictEqual(tray.icons!.trayWithBadge.constructor.name, 'NativeImage');
