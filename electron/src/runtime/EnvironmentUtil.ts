dd15afcc69465a16a68a4ffe029420fc6264ef0a
Florian Keller <florian.keller@wire.com>
2019-07-15 16:08:42 +0200

    fix: Rename dev to development

--- electron/src/runtime/EnvironmentUtil.ts
+++ electron/src/runtime/EnvironmentUtil.ts
@@ -22,7 +22,7 @@ import {settings} from '../settings/ConfigurationPersistence';
 import {SettingsType} from '../settings/SettingsType';
 
 export enum BackendTypeLabel {
-  DEV = 'Development',
+  DEVELOPMENT = 'Development',
   EDGE = 'Edge',
   INTERNAL = 'Internal',
   LOCALHOST = 'Localhost',
@@ -45,7 +45,7 @@ const URL_WEBSITE = {
 };
 
 export const URL_WEBAPP = {
-  DEV: 'https://wire-webapp-dev.zinfra.io',
+  DEVELOPMENT: 'https://wire-webapp-dev.zinfra.io',
   EDGE: 'https://wire-webapp-edge.zinfra.io',
   INTERNAL: 'https://wire-webapp-staging.wire.com/',
   LOCALHOST: 'http://localhost:8081',
