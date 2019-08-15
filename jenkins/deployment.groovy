f05b6057a1200e8e8de90da0fc67f9b49f12fbb3
Florian Keller <florian.keller@wire.com>
2019-08-15 15:17:23 +0200

    move credentials to environment

--- jenkins/deployment.groovy
+++ jenkins/deployment.groovy
@@ -73,8 +73,10 @@ node('master') {
   stage('Upload to S3 and/or Hockey') {
     withEnv(["PATH+NODE=$NODE/bin"]) {
       if (projectName.contains('Windows')) {
-        def AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
-        def AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+        environment {
+          def AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+          def AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+        }
         def HOCKEY_ID = ''
         def HOCKEY_TOKEN = ''
         def S3_BUCKET = 'wire-taco'
@@ -83,19 +85,25 @@ node('master') {
 
         try {
           if (params.Release.equals('Production')) {
-            HOCKEY_ID = credentials('WIN_PROD_HOCKEY_ID')
-            HOCKEY_TOKEN = credentials('WIN_PROD_HOCKEY_TOKEN')
+            environment {
+              HOCKEY_ID = credentials('WIN_PROD_HOCKEY_ID')
+              HOCKEY_TOKEN = credentials('WIN_PROD_HOCKEY_TOKEN')
+            }
             S3_PATH = 'win/prod'
           } else if (params.Release.equals('Custom')) {
-            AWS_ACCESS_KEY_ID = credentials("$params.AWS_CUSTOM_ACCESS_KEY_ID")
-            AWS_SECRET_ACCESS_KEY = credentials("$params.AWS_CUSTOM_SECRET_ACCESS_KEY")
-            HOCKEY_ID = credentials("$params.WIN_CUSTOM_HOCKEY_ID")
-            HOCKEY_TOKEN = credentials("$params.WIN_CUSTOM_HOCKEY_TOKEN")
+            environment {
+              AWS_ACCESS_KEY_ID = credentials("$params.AWS_CUSTOM_ACCESS_KEY_ID")
+              AWS_SECRET_ACCESS_KEY = credentials("$params.AWS_CUSTOM_SECRET_ACCESS_KEY")
+              HOCKEY_ID = credentials("$params.WIN_CUSTOM_HOCKEY_ID")
+              HOCKEY_TOKEN = credentials("$params.WIN_CUSTOM_HOCKEY_TOKEN")
+            }
             S3_BUCKET = "$params.WIN_S3_BUCKET"
             S3_PATH = "$params.WIN_S3_PATH"
           } else {
-            HOCKEY_ID = credentials('WIN_HOCKEY_TOKEN')
-            HOCKEY_TOKEN = credentials('WIN_HOCKEY_ID')
+            environment {
+              HOCKEY_ID = credentials('WIN_HOCKEY_TOKEN')
+              HOCKEY_TOKEN = credentials('WIN_HOCKEY_ID')
+            }
             S3_PATH = 'win/internal'
           }
         } catch(e) {
@@ -127,14 +135,20 @@ node('master') {
 
         try {
           if (params.Release.equals('Production')) {
-            MACOS_HOCKEY_ID = credentials('MACOS_MAS_HOCKEY_ID')
-            MACOS_HOCKEY_TOKEN = credentials('MACOS_MAS_HOCKEY_TOKEN')
+            environment {
+              MACOS_HOCKEY_ID = credentials('MACOS_MAS_HOCKEY_ID')
+              MACOS_HOCKEY_TOKEN = credentials('MACOS_MAS_HOCKEY_TOKEN')
+            }
           } else if (params.Release.equals('Custom')) {
-            MACOS_HOCKEY_ID = credentials("$params.MACOS_CUSTOM_HOCKEY_ID")
-            MACOS_HOCKEY_TOKEN = credentials("$params.MACOS_CUSTOM_HOCKEY_TOKEN")
+            environment {
+              MACOS_HOCKEY_ID = credentials("$params.MACOS_CUSTOM_HOCKEY_ID")
+              MACOS_HOCKEY_TOKEN = credentials("$params.MACOS_CUSTOM_HOCKEY_TOKEN")
+            }
           } else {
-            MACOS_HOCKEY_ID = credentials('MACOS_HOCKEY_ID')
-            MACOS_HOCKEY_TOKEN = credentials('MACOS_HOCKEY_TOKEN')
+            environment {
+              MACOS_HOCKEY_ID = credentials('MACOS_HOCKEY_ID')
+              MACOS_HOCKEY_TOKEN = credentials('MACOS_HOCKEY_TOKEN')
+            }
           }
 
           sh "npx wire-deploy-hockey --hockey-id \"$MACOS_HOCKEY_ID\" --hockey-token \"$MACOS_HOCKEY_TOKEN\" --wrapper-build \"$WRAPPER_BUILD\" --path ."
@@ -146,15 +160,19 @@ node('master') {
       } else if (projectName.contains('Linux')) {
         try {
           if (params.Release.equals('Production')) {
-            def AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
-            def AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+            environment {
+              def AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+              def AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+            }
 
             sh "npx wire-deploy-s3 --key-id \"$AWS_ACCESS_KEY_ID\" --secret-key \"$AWS_SECRET_ACCESS_KEY\" --bucket wire-taco --s3path linux --wrapper-build \"$WRAPPER_BUILD\" --path ./wrap/dist"
           } else if (params.Release.equals('Custom')) {
             // do nothing
           } else {
-            def LINUX_HOCKEY_ID = credentials('LINUX_HOCKEY_ID')
-            def LINUX_HOCKEY_TOKEN = credentials('LINUX_HOCKEY_TOKEN')
+            environment {
+              def LINUX_HOCKEY_ID = credentials('LINUX_HOCKEY_ID')
+              def LINUX_HOCKEY_TOKEN = credentials('LINUX_HOCKEY_TOKEN')
+            }
 
             sh "npx wire-deploy-hockey --hockey-id \"$LINUX_HOCKEY_ID\" --hockey-token \"$LINUX_HOCKEY_TOKEN\" --wrapper-build \"$WRAPPER_BUILD\" --path ./wrap/dist"
           }
@@ -171,8 +189,10 @@ node('master') {
     stage('Update RELEASES file') {
       try {
         withEnv(["PATH+NODE=$NODE/bin"]) {
-          def AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
-          def AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+          environment {
+            def AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+            def AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+          }
           def S3_BUCKET = 'wire-taco'
           def S3_PATH = ''
           def SEARCH_PATH = './wrap/dist/'
@@ -182,8 +202,10 @@ node('master') {
           } else if (params.Release.equals('Custom')) {
             S3_PATH = "$params.WIN_S3_PATH"
             S3_BUCKET = "$params.WIN_S3_BUCKET"
-            AWS_ACCESS_KEY_ID = credentials("$params.AWS_CUSTOM_ACCESS_KEY_ID")
-            AWS_SECRET_ACCESS_KEY = credentials("$params.AWS_CUSTOM_SECRET_ACCESS_KEY")
+            environment {
+              AWS_ACCESS_KEY_ID = credentials("$params.AWS_CUSTOM_ACCESS_KEY_ID")
+              AWS_SECRET_ACCESS_KEY = credentials("$params.AWS_CUSTOM_SECRET_ACCESS_KEY")
+            }
           } else {
             S3_PATH = 'win/internal'
           }
@@ -203,7 +225,9 @@ node('master') {
       try {
         withEnv(["PATH+NODE=$NODE/bin"]) {
           def SEARCH_PATH = './wrap/dist/'
-          def GITHUB_ACCESS_TOKEN = credentials('GITHUB_ACCESS_TOKEN')
+          environment {
+            def GITHUB_ACCESS_TOKEN = credentials('GITHUB_ACCESS_TOKEN')
+          }
 
           if (projectName.contains('macOS')) {
             SEARCH_PATH = '.'
