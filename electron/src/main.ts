ffe2df0c29cfc941bbfcd5be43bc33d367473e53
Florian Keller <florian.keller@wire.com>
2019-10-10 15:36:41 +0200

    fix double callback

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -89,7 +89,7 @@ if (argv['proxy-server-auth']) {
     authenticatedProxyInfo = new URL(argv['proxy-server-auth']);
     if (authenticatedProxyInfo.origin === 'null') {
       authenticatedProxyInfo = undefined;
-      throw new Error('No protocol specified.');
+      throw new Error('No protocol for the proxy server specified.');
     }
   } catch (error) {
     logger.error(`Could not parse authenticated proxy URL: "${error.message}"`);
@@ -103,6 +103,7 @@ let tray: TrayHandler;
 
 let isFullScreen = false;
 let isQuitting = false;
+let triedProxy = false;
 let main: BrowserWindow;
 
 Object.entries(config).forEach(([key, value]) => {
@@ -282,9 +283,9 @@ const showMainWindow = (mainWindowState: WindowStateKeeper.State) => {
 
 // App Events
 const handleAppEvents = () => {
-  app.on('window-all-closed', async () => {
+  app.on('window-all-closed', () => {
     if (!EnvironmentUtil.platform.IS_MAC_OS) {
-      await lifecycle.quit();
+      lifecycle.quit();
     }
   });
 
@@ -320,18 +321,23 @@ const handleAppEvents = () => {
         return callback(systemProxySettings.credentials.username, systemProxySettings.credentials.password);
       }
 
-      ipcMain.on(
+      ipcMain.once(
         EVENT_TYPE.PROXY_PROMPT.SUBMITTED,
         (event: IpcMessageEvent, data: {password: string; username: string}) => {
+          logger.log('Proxy prompt was submitted');
           callback(data.username, data.password);
         },
       );
 
-      ipcMain.on(EVENT_TYPE.PROXY_PROMPT.CANCELED, (event: IpcMessageEvent) => {
+      ipcMain.once(EVENT_TYPE.PROXY_PROMPT.CANCELED, () => {
+        logger.log('Proxy prompt was canceled');
         callback('', '');
       });
 
-      ProxyPromptWindow.showWindow();
+      if (!triedProxy) {
+        ProxyPromptWindow.showWindow();
+        triedProxy = true;
+      }
     }
   });
 
