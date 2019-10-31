22173c861ae4bfdbef67d1b4b39ee92b5f561a28
Florian Keller <ffflorian@users.noreply.github.com>
2019-10-31 10:16:58 +0100

    refactor: Convert webview-preload to TypeScript (#3183)

--- electron/src/interfaces/global.ts
+++ electron/src/interfaces/global.ts
@@ -17,21 +17,96 @@
  *
  */
 
+import {DesktopCapturer} from 'electron';
+import {Data as OpenGraphResult} from 'open-graph';
+
+import * as EnvironmentUtil from '../runtime/EnvironmentUtil';
 import {i18nStrings} from './locale';
 import {Schemata} from './main';
 
 declare global {
   interface Window {
+    amplify: {
+      publish(topic: string, ...args: any[]): boolean;
+      subscribe(topic: string, callback: Function, priority?: number): void;
+    };
     isMac: boolean;
     locStrings: i18nStrings;
     locStringsDefault: i18nStrings;
-    sendBadgeCount: (count: number) => void;
-    sendDeleteAccount: (accountId: string, sessionId?: string) => Promise<void>;
-    sendLogoutAccount: (accountId: string) => void;
+    sendBadgeCount(count: number): void;
+    sendDeleteAccount(accountId: string, sessionId?: string): Promise<void>;
+    sendLogoutAccount(accountId: string): void;
+    wire: any;
+    z: {
+      event: {
+        WebApp: {
+          CALL: {
+            STATE: {
+              TOGGLE: string;
+            };
+          };
+          LIFECYCLE: {
+            ASK_TO_CLEAR_DATA: string;
+            LOADED: string;
+            REFRESH: string;
+            RESTART: string;
+            SIGNED_OUT: string;
+            SIGN_OUT: string;
+            UNREAD_COUNT: string;
+            UPDATE: string;
+          };
+          NOTIFICATION: {
+            CLICK: string;
+          };
+          PREFERENCES: {
+            MANAGE_ACCOUNT: string;
+          };
+          PROPERTIES: {
+            UPDATE: {
+              INTERFACE: {
+                USE_DARK_MODE: string;
+              };
+            };
+          };
+          SHORTCUT: {
+            ADD_PEOPLE: string;
+            ARCHIVE: string;
+            DELETE: string;
+            NEXT: string;
+            NOTIFICATIONS: string;
+            PEOPLE: string;
+            PICTURE: string;
+            PING: string;
+            PREV: string;
+            SILENCE: string;
+            START: string;
+          };
+          TEAM: {
+            EVENT_FROM_BACKEND: string;
+            INFO: string;
+            MEMBER_LEAVE: string;
+            UPDATE_INFO: string;
+          };
+        };
+      };
+      lifecycle: {
+        UPDATE_SOURCE: {
+          DESKTOP: string;
+        };
+      };
+      util: {
+        Environment: {
+          version(showWrapperVersion: boolean): string;
+        };
+      };
+    };
   }
 
   namespace NodeJS {
     interface Global {
+      desktopCapturer: DesktopCapturer;
+      environment: typeof EnvironmentUtil;
+      openGraphAsync(url: string): Promise<OpenGraphResult>;
       _ConfigurationPersistence: Schemata;
     }
   }
