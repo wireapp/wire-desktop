69698e2123a4a40cb84520f3776cc1e718d9c95b
Florian Keller <florian.keller@wire.com>
2020-01-24 10:39:46 +0100

    refactor: Use autoHideMenuBar
    isMenuBarAutoHide() is deprecated

--- electron/src/menu/system.ts
+++ electron/src/menu/system.ts
@@ -140,7 +140,7 @@ const toggleMenuTemplate: MenuItemConstructorOptions = {
     const mainBrowserWindow = WindowManager.getPrimaryWindow();
 
     if (mainBrowserWindow) {
-      const showMenu = mainBrowserWindow.isMenuBarAutoHide();
+      const showMenu = mainBrowserWindow.autoHideMenuBar;
 
       mainBrowserWindow.setAutoHideMenuBar(!showMenu);
 
@@ -457,7 +457,7 @@ export const toggleMenuBar = (): void => {
 
   if (mainBrowserWindow) {
     const isVisible = mainBrowserWindow.isMenuBarVisible();
-    const autoHide = mainBrowserWindow.isMenuBarAutoHide();
+    const autoHide = mainBrowserWindow.autoHideMenuBar;
 
     if (autoHide) {
       mainBrowserWindow.setMenuBarVisibility(!isVisible);
