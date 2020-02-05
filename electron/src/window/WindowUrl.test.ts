705b799efe81bfd258a48d37ac0455f2e8bd6c7b
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-02-05 17:52:52 +0100

    Remove query string

--- electron/src/window/WindowUrl.test.ts
+++ electron/src/window/WindowUrl.test.ts
@@ -1,14 +1,33 @@
+/*
+ * Wire
+ * Copyright (C) 2020 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
 import * as assert from 'assert';
 import {WindowUrl} from './WindowUrl';
 
-describe.only('WindowUrl', () => {
+describe('WindowUrl', () => {
   describe('getUrlQueryString', () => {
     it('returns an unescaped string sequence containing the given parameters', () => {
-      const params = {
+      const params = new URLSearchParams({
         clientType: 'temporary',
         enableLogging: '@wireapp/*',
         hl: 'en',
-      };
+      });
       const queryString = WindowUrl.getQueryString(params);
       const expectedString = '?clientType=temporary&enableLogging=@wireapp/*&hl=en';
       assert.strictEqual(queryString, expectedString);
@@ -17,9 +36,9 @@ describe.only('WindowUrl', () => {
 
   describe('replaceUrlParams', () => {
     it('replaces all query params in a given url string', () => {
-      const params = {
+      const params = new URLSearchParams({
         clientType: 'overwritten',
-      };
+      });
       const url = 'https://app.wire.example.com/?clientType=temporary';
       const newUrl = WindowUrl.replaceQueryParams(url, params);
       const expectedUrl = encodeURIComponent('https://app.wire.example.com/?clientType=overwritten');
@@ -28,18 +47,6 @@ describe.only('WindowUrl', () => {
   });
 
   describe('createWebappUrl', () => {
-    it('replaces the env parameter of a local file URL with a given URL and merges the parameters', () => {
-      const localRendererUrl = `file:///D:/dev/projects/wireapp/wire-desktop/electron/renderer/index.html?env=${encodeURIComponent(
-        'https://wire-webapp-dev.zinfra.io?clientType=permanent&hl=en&enableLogging=@wireapp/*',
-      )}`;
-      const customBackendUrl = 'https://app.wire.example.com/?clientType=temporary';
-      const expectedURL = `file:///D:/dev/projects/wireapp/wire-desktop/electron/renderer/index.html?env=${encodeURIComponent(
-        'https://app.wire.example.com/?clientType=temporary&hl=en&enableLogging=@wireapp/*',
-      )}`;
-      const actual = WindowUrl.createWebappUrl(localRendererUrl, customBackendUrl, true);
-      assert.strictEqual(actual, expectedURL);
-    });
-
     it('creates a custom environment webapp URL based on parameters from an existing renderer page', () => {
       const rendererPage =
         'file:///D:/dev/projects/wireapp/wire-desktop/electron/renderer/index.html?env=https%3A%2F%2Fwire-webapp-dev.zinfra.io%3Fhl%3Den%26enableLogging%3D%40wireapp%2F*';
--- electron/src/window/WindowUrl.ts
+++ electron/src/window/WindowUrl.ts
@@ -1,38 +1,51 @@
-// TODO: Remove dependency on "querystring"
-import * as querystring from 'querystring';
+/*
+ * Wire
+ * Copyright (C) 2020 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
 
 export class WindowUrl {
-  static parseParams(url: string): querystring.ParsedUrlQuery {
+  static parseParams(url: string): URLSearchParams {
     const urlObject = new URL(url);
-    return urlObject.search ? querystring.parse(urlObject.search.substr(1)) : {};
+    return urlObject.searchParams;
   }
 
-  static getQueryString(params: querystring.ParsedUrlQuery): string {
-    return `?${querystring.unescape(querystring.stringify(params))}`;
+  static getQueryString(params: URLSearchParams): string {
+    const _params: string[] = [];
+    params.forEach((value, key) => {
+      _params.push(`${key}=${value}`);
+    });
+    return `?${_params.join('&')}`;
   }
 
-  static replaceQueryParams(url: string, params: querystring.ParsedUrlQuery): string {
+  static replaceQueryParams(url: string, params: URLSearchParams): string {
     const fullHost = url.split('?')[0];
     const unescapedQueryParams = WindowUrl.getQueryString(params);
     return encodeURIComponent(`${fullHost}${unescapedQueryParams}`);
   }
 
-  static createWebappUrl(
-    localRendererUrl: string,
-    customBackendUrl: string,
-    withRendererPage: boolean = false,
-  ): string {
+  static createWebappUrl(localRendererUrl: string, customBackendUrl: string): string {
     const localFileParams = WindowUrl.parseParams(localRendererUrl);
-    const envUrlParams = WindowUrl.parseParams(localFileParams['env'] as string);
+    const envUrlParams = WindowUrl.parseParams(localFileParams.get('env') as string);
     const customBackendUrlParams = WindowUrl.parseParams(customBackendUrl);
-    const mergedParams = {...envUrlParams, ...customBackendUrlParams};
+    const mergedParams = envUrlParams;
+    customBackendUrlParams.forEach((value, key) => {
+      mergedParams.set(key, value);
+    });
     const newEnvUrl = WindowUrl.replaceQueryParams(customBackendUrl, mergedParams);
-
-    if (withRendererPage === true) {
-      const cutPattern = '?env=';
-      const localFilePrefix = `${localRendererUrl.split(cutPattern)[0]}${cutPattern}`;
-      return `${localFilePrefix}${newEnvUrl}`;
-    }
     return newEnvUrl;
   }
 }
