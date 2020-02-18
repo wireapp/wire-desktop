5e9782586688b370b1afd8068420a9475535c6b2
Michael Koppen <michael.koppen@googlemail.com>
2020-02-18 14:36:41 +0100

    runfix: Use static functions

--- electron/renderer/src/selector/ContextMenuSelector.js
+++ electron/renderer/src/selector/ContextMenuSelector.js
@@ -1,9 +1,9 @@
 export class ContextMenuSelector {
   static getContextMenuState = state => state.contextMenuState;
-  static isEditAccountMenuVisible = state => getContextMenuState(state).isEditAccountMenuVisible;
-  static getPosition = state => getContextMenuState(state).position;
-  static getAccountId = state => getContextMenuState(state).accountId;
-  static getIsAtLeastAdmin = state => getContextMenuState(state).isAtLeastAdmin;
-  static getLifecycle = state => getContextMenuState(state).lifecycle;
-  static getSessionId = state => getContextMenuState(state).sessionId;
+  static isEditAccountMenuVisible = state => ContextMenuSelector.getContextMenuState(state).isEditAccountMenuVisible;
+  static getPosition = state => ContextMenuSelector.getContextMenuState(state).position;
+  static getAccountId = state => ContextMenuSelector.getContextMenuState(state).accountId;
+  static getIsAtLeastAdmin = state => ContextMenuSelector.getContextMenuState(state).isAtLeastAdmin;
+  static getLifecycle = state => ContextMenuSelector.getContextMenuState(state).lifecycle;
+  static getSessionId = state => ContextMenuSelector.getContextMenuState(state).sessionId;
 }
