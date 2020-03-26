656b7862d41c33bf3ee1b4f794ee8bfc29799fba
Sven Jost <sven@wire.com>
2020-03-26 13:51:31 +0100

    Disabling windows upload for now

--- jenkins/deployment.groovy
+++ jenkins/deployment.groovy
@@ -107,6 +107,7 @@ node('master') {
           wireSend secret: "$jenkinsbot_secret", message: "**Deploying to S3 failed for ${version}** see: ${JOB_URL}"
           throw e
         }
+
         if (params.Release.equals('Internal')) {
           def appName = 'Wire-Windows-Internal'
           def distributionGroups = 'All-users-of-Wire-Windows-Internal, Collaborators'
@@ -115,8 +116,9 @@ node('master') {
               zip dir: 'wrap/dist/', glob: '**/*.exe', zipFile: 'WireInternal-Setup.zip'
               files = findFiles(glob: '*.zip')
               echo("Upload " + files[0].path + " as " + appName + " to appcenter.ms...")
-              appCenter ownerName: 'Wire', apiToken: env.APP_CENTER_TOKEN, appName: appName, distributionGroups: distributionGroups, pathToApp: files[0].path, releaseNotes: 'Uploaded by Jenkins deploy job'
-              wireSend secret: "$jenkinsbot_secret", message: "**Uploaded ${files[0].path} as ${appName} ${version} to appcenter.ms**"
+              // Disabling windows upload because of missing feature to set build_version via plugin
+              // appCenter ownerName: 'Wire', apiToken: env.APP_CENTER_TOKEN, appName: appName, distributionGroups: distributionGroups, pathToApp: files[0].path, releaseNotes: 'Uploaded by Jenkins deploy job'
+              // wireSend secret: "$jenkinsbot_secret", message: "**Uploaded ${files[0].path} as ${appName} ${version} to appcenter.ms**"
             }
           } catch(e) {
             currentBuild.result = 'FAILED'
