bdb5fdcf940f23cf30784b5ce02436dc0bf6d9bb
Florian Keller <florian.keller@wire.com>
2020-04-30 17:28:08 +0200

    fix: Log web console messages to file

--- electron/src/auth/ProxyAuth.ts
+++ electron/src/auth/ProxyAuth.ts
@@ -35,12 +35,12 @@ interface ProxyOptions {
   username?: string;
 }
 
-function generateProxyURL(authInfo: AuthInfo, options: ProxyOptions): URL {
+export function generateProxyURL(authInfo: AuthInfo, options: ProxyOptions): URL {
   let protocol = options.protocol;
   const {username, password} = options;
 
   if (!protocol) {
-    logger.log('Default to HTTP proxy');
+    logger.log('Defaulting to HTTP proxy');
     protocol = 'http';
   }
 
@@ -55,6 +55,7 @@ function generateProxyURL(authInfo: AuthInfo, options: ProxyOptions): URL {
     proxySettings.username = username;
     logger.log('Username set');
   }
+
   if (password) {
     proxySettings.password = password;
     logger.log('Password set');
@@ -62,9 +63,3 @@ function generateProxyURL(authInfo: AuthInfo, options: ProxyOptions): URL {
 
   return proxySettings;
 }
-
-const ProxyAuth = {
-  generateProxyURL,
-};
-
-export default ProxyAuth;
--- electron/src/main.ts
+++ electron/src/main.ts
@@ -37,6 +37,8 @@ import * as logdown from 'logdown';
 import * as minimist from 'minimist';
 import * as path from 'path';
 import {URL} from 'url';
+import WindowStateKeeper = require('electron-window-state');
+import fileUrl = require('file-url');
 
 import {
   attachTo as attachCertificateVerifyProcManagerTo,
@@ -64,9 +66,7 @@ import {AboutWindow} from './window/AboutWindow';
 import {ProxyPromptWindow} from './window/ProxyPromptWindow';
 import {WindowManager} from './window/WindowManager';
 import {WindowUtil} from './window/WindowUtil';
-import ProxyAuth from './auth/ProxyAuth';
-import WindowStateKeeper = require('electron-window-state');
-import fileUrl = require('file-url');
+import * as ProxyAuth from './auth/ProxyAuth';
 
 const APP_PATH = path.join(app.getAppPath(), config.electronDirectory);
 const INDEX_HTML = path.join(APP_PATH, 'renderer/index.html');
@@ -363,7 +363,7 @@ const handleAppEvents = () => {
             logger.log('Proxy prompt was submitted');
 
             const {username, password} = promptData;
-            const protocol: string | undefined = proxyInfoArg?.protocol?.replace(':', '');
+            const protocol = proxyInfoArg?.protocol?.replace(':', '');
             proxyInfoArg = ProxyAuth.generateProxyURL(
               {host, port},
               {
@@ -476,7 +476,7 @@ class ElectronWrapperInit {
   logger: logdown.Logger;
 
   constructor() {
-    this.logger = LogFactory.getLogger('ElectronWrapperInit', {logFilePath: LOG_FILE});
+    this.logger = getLogger('ElectronWrapperInit');
   }
 
   async run(): Promise<void> {
@@ -499,7 +499,7 @@ class ElectronWrapperInit {
         return new SingleSignOn(main, event, url, options).init();
       }
 
-      this.logger.log(`Opening an external window from a webview.`);
+      this.logger.log('Opening an external window from a webview.');
       return shell.openExternal(url);
     };
 
@@ -544,11 +544,10 @@ class ElectronWrapperInit {
             contents.on('console-message', async (_event, _level, message) => {
               const webViewId = lifecycle.getWebViewId(contents);
               if (webViewId) {
-                const logFilePath = path.join(LOG_DIR, webViewId, config.logFileName);
                 try {
-                  await LogFactory.writeMessage(message, logFilePath);
+                  await LogFactory.writeMessage(message.replace(/%c(.+)%c /, '$1 '), LOG_FILE);
                 } catch (error) {
-                  logger.error(`Cannot write to log file "${logFilePath}": ${error.message}`, error);
+                  logger.error(`Cannot write to log file "${LOG_FILE}": ${error.message}`, error);
                 }
               }
             });
