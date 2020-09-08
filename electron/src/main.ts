94b6d13c77995df7efdacecbc80a9e911d2b89d2
Florian Imdahl <ffflorian@users.noreply.github.com>
2020-09-08 15:30:16 +0200

    chore: Disable experimental Chrome features (#4248)

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -532,7 +532,7 @@ class ElectronWrapperInit {
             params.contextIsolation = 'true';
             params.plugins = 'false';
             webPreferences.allowRunningInsecureContent = false;
-            webPreferences.experimentalFeatures = true;
+            webPreferences.experimentalFeatures = false;
             webPreferences.nodeIntegration = false;
             webPreferences.preload = PRELOAD_RENDERER_JS;
             webPreferences.spellcheck = enableSpellChecking;
