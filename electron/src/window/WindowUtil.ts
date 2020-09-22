faa821c5f6533ba10b2f113d5500b78fe4dd5559
Florian Imdahl <florian.keller@wire.com>
2020-09-22 15:00:22 +0200

    chore: Update translations

--- electron/src/window/WindowUtil.ts
+++ electron/src/window/WindowUtil.ts
@@ -24,6 +24,7 @@ import * as URL from 'url';
 import {getLogger} from '../logging/getLogger';
 import {showWarningDialog} from '../lib/showDialog';
 import {config} from '../settings/config';
+import * as locale from '../locale/locale';
 
 const logger = getLogger(path.basename(__filename));
 
@@ -66,7 +67,7 @@ export const openExternal = async (url: string, httpsOnly: boolean = false): Pro
 
     if (!allowedProtocols.includes(urlProtocol)) {
       logger.warn(`Prevented opening external URL "${url}".`);
-      showWarningDialog(`Prevented opening external URL "${url}".`);
+      showWarningDialog(locale.getText('urlBlockedPromptText'));
       return;
     }
 
