98c1dae7153d09191500bb7607f5c0670105c683
Florian Keller <florian.keller@wire.com>
2021-03-02 14:06:34 +0100

    fix color

--- electron/renderer/src/components/IsLoading.jsx
+++ electron/renderer/src/components/IsLoading.jsx
@@ -1,6 +1,6 @@
 /*
  * Wire
- * Copyright (C) 2018 Wire Swiss GmbH
+ * Copyright (C) 2021 Wire Swiss GmbH
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,7 +39,7 @@ const IsLoading = ({webviewRef}) => {
     isLoading && (
       <div
         style={{
-          backgroundColor: 'rgb(247, 248, 250)',
+          backgroundColor: '#f7f8fa',
           display: 'flex',
           height: '100%',
           position: 'absolute',
