6b66d6b670096386b9a2f4d1adcf11e72a015183
Florian Imdahl <florian@wire.com>
2021-07-07 11:08:37 +0200

    only check .app for notarization

--- bin/macos-check_notarization.sh
+++ bin/macos-check_notarization.sh
@@ -1,26 +1,18 @@
 #!/usr/bin/env bash
 
-if ! command -v stapler > /dev/null; then
-  echo "Could not find otool. Exiting."
+if ! command -v spctl > /dev/null; then
+  echo "Could not find spctl. Exiting."
   exit
 fi
 
-WIRE_PKG_FILE="wrap/dist/Wire.pkg"
-WIRE_APP_FILE="wrap/build/Wire-mas-x64/Wire.app"
+WIRE_APP_FILE="${1:-"wrap/build/Wire-mas-x64/Wire.app"}"
 
-STAPLER_PKG_RESULT="$(stapler validate "${WIRE_PKG_FILE}" 2>&1)"
-SPCTL_PKG_RESULT="$(spctl -a -vvv -t install "${WIRE_PKG_FILE}" 2>&1)"
-STAPLER_APP_RESULT="$(stapler validate "${WIRE_APP_FILE}" 2>&1)"
-SPCTL_APP_RESULT="$(spctl -a -vvv -t install "${WIRE_APP_FILE}" 2>&1)"
+SPCTL_RESULT="$(spctl -a -vvv -t install "${WIRE_APP_FILE}" 2>&1)"
 
-echo "Stapler .pkg notarization result:"
-echo "${STAPLER_PKG_RESULT}"
-echo
-echo "spctl .pkg notarization result:"
-echo "${SPCTL_PKG_RESULT}"
-echo
-echo "Stapler .app notarization result:"
-echo "${STAPLER_APP_RESULT}"
-echo
-echo "spctl .app notarization result:"
-echo "${SPCTL_APP_RESULT}"
+if [ "${SPCTL_RESULT}" == *"rejected"* ]; then
+  echo -e "⚠️ **Warning**: The build was not correctly notarized:"
+else
+  echo -e "👍 The build was correctly notarized:"
+fi
+
+echo "${SPCTL_RESULT}"
