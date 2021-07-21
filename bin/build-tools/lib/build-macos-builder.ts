2176848c9866e2366d1d4c86ff271f77d27c26e3
Florian Imdahl <florian@wire.com>
2021-07-21 14:36:46 +0200

    remove timestamp

--- bin/build-tools/lib/build-macos-builder.ts
+++ bin/build-tools/lib/build-macos-builder.ts
@@ -116,7 +116,8 @@ export async function buildMacOSConfig(
       identity: macOSConfig.certName,
       strictVerify: 'all',
       target: macOSConfig.enableNotarization ? ['dmg', 'mas'] : 'mas',
-    },
+      timestamp: 'none',
+    } as any,
     mas: {
       entitlements: path.resolve('resources/macos/entitlements/parent.plist'),
       entitlementsInherit: path.resolve('resources/macos/entitlements/child.plist'),
