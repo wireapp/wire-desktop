145b8f665dcdae882362e747af757f0c56bde55b
Florian Imdahl <florian@wire.com>
2021-09-06 18:00:54 +0200

    chore: Use mas specific plist files for signing manually

--- bin/build-tools/lib/build-macos.ts
+++ bin/build-tools/lib/build-macos.ts
@@ -185,8 +185,8 @@ export async function manualMacOSSign(
   commonConfig: CommonConfig,
   macOSConfig: MacOSConfig,
 ): Promise<void> {
-  const inheritEntitlements = 'resources/macos/entitlements/child.plist';
-  const mainEntitlements = 'resources/macos/entitlements/parent.plist';
+  const inheritEntitlements = 'resources/macos/entitlements/child-mas.plist';
+  const mainEntitlements = 'resources/macos/entitlements/parent-mas.plist';
 
   if (macOSConfig.certNameApplication) {
     const filesToSign = [
