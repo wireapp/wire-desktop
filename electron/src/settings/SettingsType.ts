50aa8012583c8f67558a694564d507fa8277f907
Benny Neugebauer <mail@bennycode.com>
2021-09-10 17:58:55 +0200

    docs: Document configuration file (init.json) properties (#5309)

--- electron/src/settings/SettingsType.ts
+++ electron/src/settings/SettingsType.ts
@@ -17,14 +17,29 @@
  *
  */
 
+/**
+ * Settings that can be set in Wire's "init.json" file to configure our desktop app.
+ *
+ * Our configuration file can be found here:
+ * - Windows: %APPDATA%\Wire\config\init.json
+ */
 export enum SettingsType {
+  /** Start Wire on OS startup? */
   AUTO_LAUNCH = 'shouldAutoLaunch',
+  /** Custom web app URL to use in on-premise deployments. The "env" setting must be set to "CUSTOM" to use this. */
   CUSTOM_WEBAPP_URL = 'customWebAppURL',
+  /** Enable spell checker in desktop app? */
   ENABLE_SPELL_CHECKING = 'enableSpellChecking',
+  /** Which cloud environment ("PRODUCTION", "INTERNAL", "AVS", ...) to use when loading the webapp? */
   ENV = 'env',
+  /** Start Wire desktop app in fullscreen? */
   FULL_SCREEN = 'fullscreen',
+  /** Which language (ISO 639-1) should be used to load our web app (de, en, fr, etc.)? */
   LOCALE = 'locale',
+  /** Show menu bar for desktop application? */
   SHOW_MENU_BAR = 'showMenu',
+  /** At which part of the screen shall the app be initially rendered? */
   WINDOW_BOUNDS = 'bounds',
+  /** Factor for "Zoom In" our "Zoom Out" (makes our UI easier to read). */
   ZOOM_FACTOR = 'zoomFactor',
 }
