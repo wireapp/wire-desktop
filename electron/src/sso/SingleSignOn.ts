c429145f9605a9d686213fe244a813c36e7f10aa
Sebastian Willenborg <sebastian.willenborg@wire.com>
2022-08-18 15:30:48 +0200

    fix: prevent connections by electron on link clicks (SQCORE-1348) (#5910)

--- electron/src/sso/SingleSignOn.ts
+++ electron/src/sso/SingleSignOn.ts
@@ -179,7 +179,9 @@ export class SingleSignOn {
 
     // Prevent title updates and new windows
     ssoWindow.on('page-title-updated', event => event.preventDefault());
-    ssoWindow.webContents.on('new-window', event => event.preventDefault());
+    ssoWindow.webContents.setWindowOpenHandler(details => {
+      return {action: 'deny'};
+    });
 
     ssoWindow.webContents.on('will-navigate', (event: ElectronEvent, url: string) => {
       const {origin} = new URL(url);
