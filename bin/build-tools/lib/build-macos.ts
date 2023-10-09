feb21287744a066ea760f38ac61b1f79d3654ea6
Thomas Belin <me@thomasbelin.fr>
2023-10-09 16:23:57 +0200

    chore: Fix macos signing (#7126)

--- bin/build-tools/lib/build-macos.ts
+++ bin/build-tools/lib/build-macos.ts
@@ -111,15 +111,15 @@ export async function buildMacOSConfig(
   if (!signManually) {
     if (macOSConfig.certNameApplication) {
       packagerConfig.osxSign = {
-        entitlements: 'resources/macos/entitlements/parent.plist',
-        'entitlements-inherit': 'resources/macos/entitlements/child.plist',
+        optionsForFile: () => ({
+          entitlements: 'resources/macos/entitlements/parent.plist',
+        }),
         identity: macOSConfig.certNameApplication,
       };
     }
 
     if (macOSConfig.notarizeAppleId && macOSConfig.notarizeApplePassword) {
-      // once https://github.com/electron/electron-packager/issues/1162 is fixed, any can be removed
-      (packagerConfig as any).osxNotarize = {
+      packagerConfig.osxNotarize = {
         appleId: macOSConfig.notarizeAppleId,
         appleIdPassword: macOSConfig.notarizeApplePassword,
       };
