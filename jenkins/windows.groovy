bd2f20eaa1d64cf6f9a2c9ba41ecc7a4b336cf98
v-gira <virgile.girardin@gmail.com>
2025-04-03 13:48:22 +0200

    run the pipelline with signing only

--- jenkins/windows.groovy
+++ jenkins/windows.groovy
@@ -31,38 +31,38 @@ node('windows') {
     def electronVersion = parseJson(packageJson).devDependencies.electron
     currentBuild.displayName = version
 
-    stage('Build') {
-      try {
-        withEnv(["PATH+NODE=${NODE}", 'npm_config_target_arch=x64']) {
-          bat 'node -v'
-          bat 'npm -v'
-          bat 'npm install -g yarn'
-          bat 'yarn'
-          bat 'yarn build:win'
-        }
-    } catch (e) {
-        currentBuild.result = 'FAILED'
-        wireSend secret: "${jenkinsbot_secret}", message: "🏞 **${JOB_NAME} ${version} build failed**\n${BUILD_URL}"
-        throw e
-      }
-    }
+    // stage('Build') {
+    //   try {
+    //     withEnv(["PATH+NODE=${NODE}", 'npm_config_target_arch=x64']) {
+    //       bat 'node -v'
+    //       bat 'npm -v'
+    //       bat 'npm install -g yarn'
+    //       bat 'yarn'
+    //       bat 'yarn build:win'
+    //     }
+    // } catch (e) {
+    //     currentBuild.result = 'FAILED'
+    //     wireSend secret: "${jenkinsbot_secret}", message: "🏞 **${JOB_NAME} ${version} build failed**\n${BUILD_URL}"
+    //     throw e
+    //   }
+    // }
 
-    stage('Build installer') {
-      try {
-        withEnv(["PATH+NODE=${NODE}", 'npm_config_target_arch=x64']) {
-          bat 'yarn build:win:installer'
-        }
-    } catch (e) {
-        currentBuild.result = 'FAILED'
-        wireSend secret: "${jenkinsbot_secret}", message: "🏞 **${JOB_NAME} ${version} building installer failed**\n${BUILD_URL}"
-        throw e
-      }
-    }
+    // stage('Build installer') {
+    //   try {
+    //     withEnv(["PATH+NODE=${NODE}", 'npm_config_target_arch=x64']) {
+    //       bat 'yarn build:win:installer'
+    //     }
+    // } catch (e) {
+    //     currentBuild.result = 'FAILED'
+    //     wireSend secret: "${jenkinsbot_secret}", message: "🏞 **${JOB_NAME} ${version} building installer failed**\n${BUILD_URL}"
+    //     throw e
+    //   }
+    // }
 
     stage('Sign installer') {
       withCredentials([string(credentialsId: 'SM_API_KEY', variable: 'SM_API_KEY'), string(credentialsId: 'SM_HOST', variable: 'SM_HOST'), string(credentialsId: 'SM_CLIENT_CERT_PASSWORD', variable: 'SM_CLIENT_CERT_PASSWORD'), file(credentialsId: 'SM_CLIENT_CERT_FILE', variable: 'SM_CLIENT_CERT_FILE'), string(credentialsId: 'SM_KEYPAIR_ALIAS', variable: 'SM_KEYPAIR_ALIAS')]) {
         try {
-          bat 'for %%f in ("wrap\\dist\\*-Setup.exe") do (smctl sign --keypair-alias %SM_KEYPAIR_ALIAS% --config-file %SM_CLIENT_CERT_FILE% --input %%f -v)'
+          bat 'for %%f in (" C:\\Users\\jenkins\\Downloads\\*-Setup.exe") do (smctl sign --keypair-alias %SM_KEYPAIR_ALIAS% --config-file %SM_CLIENT_CERT_FILE% --input %%f -v)'
           } catch (e) {
           currentBuild.result = 'FAILED'
           wireSend secret: "${jenkinsbot_secret}", message: "🏞 **${JOB_NAME} ${version} signing installer failed**\n${BUILD_URL}"
@@ -80,32 +80,32 @@ node('windows') {
         wireSend secret: "${jenkinsbot_secret}", message: "🏞 **${JOB_NAME} ${version} verifying installer failed**\n${BUILD_URL}"
       }
     }
-      stage('Archive build artifacts') {
-        archiveArtifacts 'package.json,wrap\\dist\\**'
-      }
+    //   stage('Archive build artifacts') {
+    //     archiveArtifacts 'package.json,wrap\\dist\\**'
+    //   }
 
-      stage('Print hash') {
-        try {
-          if (production) {
-            bat 'certUtil -hashfile "wrap\\dist\\Wire-Setup.exe" SHA256'
-          }
-    } catch (e) {
-          currentBuild.result = 'FAILED'
-          wireSend secret: "${jenkinsbot_secret}", message: "🏞 **${JOB_NAME} ${version} printing hash failed**\n${BUILD_URL}"
-          throw e
-        }
-      }
+    //   stage('Print hash') {
+    //     try {
+    //       if (production) {
+    //         bat 'certUtil -hashfile "wrap\\dist\\Wire-Setup.exe" SHA256'
+    //       }
+    // } catch (e) {
+    //       currentBuild.result = 'FAILED'
+    //       wireSend secret: "${jenkinsbot_secret}", message: "🏞 **${JOB_NAME} ${version} printing hash failed**\n${BUILD_URL}"
+    //       throw e
+    //     }
+    //   }
 
-      stage('Trigger smoke tests') {
-        if (production) {
-          try {
-            build job: 'Wrapper_Windows_Smoke_Tests', parameters: [run(description: '', name: 'WRAPPER_BUILD', runId: "Wrapper_Windows_Production#${BUILD_ID}"), string(name: 'WEBAPP_ENV', value: 'https://wire-webapp-master.zinfra.io/')], wait: false
-      } catch (e) {
-            wireSend secret: "${jenkinsbot_secret}", message: "🏞 **${JOB_NAME} Unable to trigger smoke tests for ${version}**\n${BUILD_URL}"
-            print e
-          }
-        }
-      }
+    //   stage('Trigger smoke tests') {
+    //     if (production) {
+    //       try {
+    //         build job: 'Wrapper_Windows_Smoke_Tests', parameters: [run(description: '', name: 'WRAPPER_BUILD', runId: "Wrapper_Windows_Production#${BUILD_ID}"), string(name: 'WEBAPP_ENV', value: 'https://wire-webapp-master.zinfra.io/')], wait: false
+    //   } catch (e) {
+    //         wireSend secret: "${jenkinsbot_secret}", message: "🏞 **${JOB_NAME} Unable to trigger smoke tests for ${version}**\n${BUILD_URL}"
+    //         print e
+    //       }
+    //     }
+    //   }
 
-      wireSend secret: "${jenkinsbot_secret}", message: "🏞 **New build of ${JOB_NAME} ${version}**\n- Download: [Jenkins](${BUILD_URL})\n- Electron version: ${electronVersion}\n- Branch: [${GIT_BRANCH}](https://github.com/wireapp/wire-desktop/commits/${GIT_BRANCH})"
+    //   wireSend secret: "${jenkinsbot_secret}", message: "🏞 **New build of ${JOB_NAME} ${version}**\n- Download: [Jenkins](${BUILD_URL})\n- Electron version: ${electronVersion}\n- Branch: [${GIT_BRANCH}](https://github.com/wireapp/wire-desktop/commits/${GIT_BRANCH})"
 }
