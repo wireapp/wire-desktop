fefb637e430d435a23674173afc4d0ac38c3522f
Vitaly Kuprin <vitas_s@inbox.lv>
2025-09-29 13:03:59 +0200

    chore: temp

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -161,6 +161,7 @@ let isQuitting = false;
 let main: BrowserWindow;
 
 (async () => {
+  // NOSONAR - TypeScript config doesn't support top-level await
   for (const [key, value] of Object.entries(config)) {
     if (value === undefined || (typeof value === 'number' && Number.isNaN(value))) {
       logger.warn(`Configuration key "${key}" not defined.`);
