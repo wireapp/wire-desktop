5bef98a848a2e1847d0f1d502469555133cb3d4a
Mark Brockhoff <mark.brockhoff@digits.schwarz>
2026-06-09 15:01:11 +0200

    set default test id attribuge

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    testIdAttribute: 'data-uie-name',
   },
 
   /* Configure projects for major browsers */
