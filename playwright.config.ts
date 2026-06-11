da00b2ff29f839248e46a4eee4dbdc3e47916d43
Mark Brockhoff <mark.brockhoff@digits.schwarz>
2026-06-11 11:00:12 +0200

    increase test timeout to 90s

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,6 +39,7 @@ export default defineConfig({
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  timeout: 90_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
