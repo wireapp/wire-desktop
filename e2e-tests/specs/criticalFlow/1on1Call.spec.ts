075c68ebb371ec4afaa661e370ee519901f2866f
Mark Brockhoff <mark.brockhoff@digits.schwarz>
2026-06-16 15:39:12 +0200

    Implement critical flow test for 1on1 call [WPB-26068]

---
+++ e2e-tests/specs/criticalFlow/1on1Call.spec.ts
@@ -0,0 +1,54 @@
+/*
+ * Wire
+ * Copyright (C) 2026 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {connectWithUser} from '../../actions/connectWithUser';
+import {loginUser} from '../../actions/loginUser';
+import {expect, test} from '../../fixtures';
+import {callingPage} from '../../poms/webapp/calling.page';
+import {conversation} from '../../poms/webapp/conversation.page';
+import {conversationsList} from '../../poms/webapp/conversationList.page';
+
+test(
+  'I want to call someone directly',
+  {tag: ['@TC-10926', '@crit-flow-desktop']},
+  async ({page: userAPage, createUser, createTeam, createPage}) => {
+    const userB = await createUser();
+    const {owner: userA} = await createTeam('Test Team', {users: [userB]});
+    const userBPage = await createPage();
+
+    await Promise.all([loginUser(userAPage, userA), loginUser(userBPage, userB)]);
+    await connectWithUser(userAPage, userB);
+
+    await test.step('User A calls user B', async () => {
+      await conversationsList(userAPage).getConversation(userB.fullName).open();
+      await conversation(userAPage).startCallButton.click();
+      await expect(callingPage(userAPage).callCell).toBeVisible();
+    });
+
+    await test.step('User B sees call and picks up', async () => {
+      await expect(callingPage(userBPage).callCell).toBeVisible();
+      await callingPage(userBPage).acceptCallButton.click();
+    });
+
+    await test.step('A and B should see the call with each other', async () => {
+      await expect(callingPage(userAPage).goFullScreen).toBeVisible();
+      await expect(callingPage(userBPage).goFullScreen).toBeVisible();
+    });
+  },
+);
