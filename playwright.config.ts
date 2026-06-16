1d871f1fbb301c2252df32a6eac10514b3fef54a
Mark Brockhoff <mark.brockhoff@digits.schwarz>
2026-06-16 12:09:11 +0200

    Set up generation of playwright reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,8 +37,14 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests because there can only be one running instance of the app at a time */
   workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // Only generate reports on CI
+  reporter: process.env.CI
+    ? [
+        ['html', {outputFolder: 'playwright-report/html', open: 'never'}],
+        ['json', {outputFile: 'playwright-report/report.json'}],
+        ['line'],
+      ]
+    : 'line',
   timeout: 90_000,
   expect: {timeout: 10_000},
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
