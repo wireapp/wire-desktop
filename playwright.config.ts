8c7175bc8211592f4915de6cb0cbf4f794b0ad38
zhanna_chaikovska <zhanna.chaykovska@gmail.com>
2026-06-18 15:50:02 +0200

    test: bypass media permission prompts for calls

--- playwright.config.ts
+++ playwright.config.ts
@@ -53,13 +53,23 @@ export default defineConfig({
     trace: 'retain-on-first-failure',
     testIdAttribute: 'data-uie-name',
     baseURL: process.env.WEBAPP_URL,
+    permissions: ['camera', 'microphone'],
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: {...devices['Desktop Chrome']},
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          args: [
+            '--use-fake-device-for-media-stream', // Provide fake devices for audio & video device input
+            '--use-fake-ui-for-media-stream', // Bypasses the popup to grant permission and select video / audio input device by automatically selecting the default one
+            '--mute-audio', // Mute all audio output from the test browser because e.g. the ringtone of a call can be annoying during testing
+          ],
+        },
+      },
     },
   ],
 });
