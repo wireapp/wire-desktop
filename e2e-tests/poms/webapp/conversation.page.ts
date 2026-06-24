5d2226b5e910ca129971281f64a9c1bbdf325494
Mark Brockhoff <mark.brockhoff@digits.schwarz>
2026-06-24 11:56:20 +0200

    Add utils for sending and reading messages inside a conversation

--- e2e-tests/poms/webapp/conversation.page.ts
+++ e2e-tests/poms/webapp/conversation.page.ts
@@ -19,10 +19,53 @@
 
 import {Page} from '@playwright/test';
 
+import {User} from '../../actions/createUser';
+
 export const conversation = (page: Page) => {
-  const startCallButton = page.locator('[data-uie-name="do-call"]');
+  const startCallButton = page.getByTestId('do-call');
+
+  /**
+   * The attribute 'send-status' will be 1 while the message is being sent, since we only want to assert on sent messages these messages will be excluded. See: {@see StatusTypes}
+   * Status type -1 ensures that system messages do NOT count as sent messages
+   */
+  const messages = page.locator(
+    `[data-uie-name="item-message"]:not([data-uie-send-status="1"]):not([data-uie-send-status="-1"]):not(.system-message)`,
+  );
+
+  const messageInput = page.getByTestId('input-message');
+  const sendMessageButton = page.getByRole('button', {name: 'Send message'});
+
+  /* Send a message into the currently open conversation */
+  const sendMessage = async (message: string) => {
+    await messageInput.fill(message);
+    await sendMessageButton.click();
+  };
+
+  // eslint-disable-next-line valid-jsdoc
+  /**
+   * Util to get a message in the conversation
+   * @returns a Locator to the matching message(s)
+   */
+  const getMessage = (options?: {content?: string | RegExp; sender?: User}) => {
+    let message = messages;
+
+    if (options?.content) {
+      message = message.filter({hasText: options.content});
+    }
+
+    if (options?.sender?.fullName) {
+      message = message.filter({
+        // Using getByLabel doesn't work here as the aria label is just placed on a div with no input inside which could be located
+        has: page.locator(`.content-message-wrapper[aria-label*="${options.sender.fullName}"]`),
+      });
+    }
+
+    return message;
+  };
 
   return {
     startCallButton,
+    sendMessage,
+    getMessage,
   };
 };
